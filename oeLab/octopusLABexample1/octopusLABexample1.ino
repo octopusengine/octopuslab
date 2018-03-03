50ee2abfa67d5feec3b206c3fe6aec6171e646cd
Honza S. Copak <bonum.comunae@volny.cz>
2018-03-03 17:24:13 +0100

    Add files via upload

---
+++ oeLab/octopusLABexample1/octopusLABexample1.ino
@@ -0,0 +1,26 @@
+#include "octopusLAB.h"
+
+DigitalPin led(OE_LAB_LED);
+DigitalPin piezzo(OE_LAB_PIEZZO);
+//Morse morse(2);
+
+void setup()
+{
+  /*morse.dot(); morse.dot(); morse.dot();
+  morse.dash(); morse.dash(); morse.dash();
+  morse.dot(); morse.dot(); morse.dot();
+  */
+}
+
+void loop()
+{
+  led.SetLow();
+  piezzo.Beep(20);
+  delay(500);
+  led.SetHigh();
+  
+  delay(3000);
+  led.FastBlink(7); //test signal alert
+  delay(2000);
+  piezzo.BeepNote(); //test - need PWM!
+}
