24f0031dc47c4b6cdc52fef9680538822475b569
Honza S. Copak <bonum.comunae@volny.cz>
2018-03-18 15:29:03 +0100

    Update octopusLABexample2beep.ino

--- oeLab/octopusLABexample2beep/octopusLABexample2beep.ino
+++ oeLab/octopusLABexample2beep/octopusLABexample2beep.ino
@@ -2,33 +2,19 @@
 
 DigitalPin led(OE_LAB_LED);
 DigitalPin piezo(OE_LAB_PIEZZO2);
-//Morse morse(2);
 
 void setup()
-{
-  /*morse.dot(); morse.dot(); morse.dot();
-  morse.dash(); morse.dash(); morse.dash();
-  morse.dot(); morse.dot(); morse.dot();
-  */
-  //test kHz
-  //piezo.Beep(100);
-  //delay(200);
-  ///ok piezo.playMelody1();  
+{  
 }
 
 void loop()
 {
   led.SetLow();
-  //piezo.Beep(20);
   delay(500);
   led.SetHigh();
   
   delay(3000);
-  led.FastBlink(7); //test signal alert
+  led.FastBlink(7); 
   delay(1000);
-  //piezo.BeepNote1(); //test - need PWM!
-  // delay(1000);
-  //piezo.BeepNote2();
-   piezo.playMelody2();  
-  
+  piezo.playMelody2();   
 }
