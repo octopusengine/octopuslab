72917b39a02bf009497fb8edb587af6bf61d6016
Honza S. Copak <bonum.comunae@volny.cz>
2018-08-09 18:37:37 +0200

    Update attiny-02-serial.ino

--- attiny/attiny-projects/attiny-02-serial/attiny-02-serial.ino
+++ attiny/attiny-projects/attiny-02-serial/attiny-02-serial.ino
@@ -16,14 +16,12 @@ Octopus engine - oeLAB - 2018/07 TEST
 #define TX    4   // *** D4
 SoftwareSerial TSerial(RX, TX); //TSerial for Attiny
 
-int ledR = 1;
-int ledB = 1;
-// the setup function runs once when you press reset or power the board
-
+#define LED1_PIN        1              
+#define LED2_PIN        3  
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(ledR, OUTPUT); //LED_BUILTIN
+  pinMode(LED1_PIN, OUTPUT); //LED_BUILTIN
   //pinMode(ledB, OUTPUT);
   //digitalWrite(ledB, LOW);
   
@@ -50,10 +48,10 @@ void setup() {
 int cnt = 0;
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(ledR, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED1_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   //digitalWrite(ledB, LOW);
   delay(200);                       // wait for a second
-  digitalWrite(ledR, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED1_PIN, LOW);    // turn the LED off by making the voltage LOW
   //digitalWrite(ledB, HIGH);
   delay(300);                      // wait for a second
 
