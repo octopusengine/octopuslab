5f7867f110363dfea4817452a6d2409e158c81a9
Honza S. Copak <bonum.comunae@volny.cz>
2018-08-09 18:44:28 +0200

    Update attiny-01-blink.ino

--- attiny/attiny-projects/attiny-01-blink/attiny-01-blink.ino
+++ attiny/attiny-projects/attiny-01-blink/attiny-01-blink.ino
@@ -8,15 +8,12 @@ Octopus engine - oeLAB - 2018/07 TEST
         /Tx (A) P4 =     = P1 /    (2) > LED 
                GND =     = P0 /  > (1) i2c Data 
 */
-#define LED1_PIN        1              
-#define LED2_PIN        3  
-//int ledR = 1;
-//int ledB = 3;
+#define LED1_PIN        1  //LED_BUILTIN = Led2            
+#define LED2_PIN        3  //
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED1_PIN, OUTPUT); //LED_BUILTIN
+void setup() {  
+  pinMode(LED1_PIN, OUTPUT); // initialize digital pin as an output
   pinMode(LED2_PIN, OUTPUT);
 }
 
@@ -24,8 +21,8 @@ void setup() {
 void loop() {
   digitalWrite(LED1_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED2_PIN, LOW);
-  delay(1000);                       // wait for a second
+  delay(1000);                    // wait for a second
   digitalWrite(LED1_PIN, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(LED2_PIN, HIGH);
-  delay(1000);                       // wait for a second
+  delay(1000);                       
 }
