7126e31ead0e5608729cffd221fd573df4428a12
Honza S. Copak <bonum.comunae@volny.cz>
2018-09-12 10:47:19 +0200

    Update esp32-01-blink.ino

--- esp32/esp32-01-blink/esp32-01-blink.ino
+++ esp32/esp32-01-blink/esp32-01-blink.ino
@@ -1,8 +1,5 @@
-// Blikání LED s ESP32
-
-/*
-  Blink
-*/
+// základní test funkčnosti a programátoru: Blikání LED s ESP32
+// esp32-01-blink - simple test: LED_BUILTIN
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -12,8 +9,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(300);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(300);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait 
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(300);                       // wait
 }
