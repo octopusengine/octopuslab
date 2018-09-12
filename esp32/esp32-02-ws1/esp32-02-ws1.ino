bdf30ac98f2778de774b05924677f2f9daa9ce97
Honza S. Copak <bonum.comunae@volny.cz>
2018-09-12 09:43:25 +0200

    Update esp32-02-ws1.ino

--- esp32/esp32-02-ws1/esp32-02-ws1.ino
+++ esp32/esp32-02-ws1/esp32-02-ws1.ino
@@ -1,28 +1,21 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+//esp32-02-ws1 - simple test: one WS2812 rgb LED
+//octopusLAB - ESP32 ROBOT board (2018/09)
+//NeoPixel(c) 2013 Shae Erisson (AdaFruit NeoPixel library)
 
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
 
 #define PIN_WS         13
 #define NUMPIXELS      1
+Adafruit_NeoPixel wsLED = Adafruit_NeoPixel(NUMPIXELS, PIN_WS, NEO_GRB + NEO_KHZ800);
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN_WS, NEO_GRB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
+int delayval = 2000; // delay for half a second
 
 void setup() {
-  pixels.begin(); // This initializes the NeoPixel library.
+  wsLED.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
-  /*
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+/*
   for(int i=0;i<NUMPIXELS;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(150,0,0));
@@ -30,15 +23,15 @@ void loop() {
     delay(delayval);
     }
 */
-    pixels.setPixelColor(0, pixels.Color(150,0,0));
-    pixels.show();
+    wsLED.setPixelColor(0, pixels.Color(150,0,0));
+    wsLED.show();
     delay(delayval);
     
-    pixels.setPixelColor(0, pixels.Color(0,150,0));
-    pixels.show();
+    wsLED.setPixelColor(0, pixels.Color(0,150,0));
+    wsLED.show();
     delay(delayval);
 
-    pixels.setPixelColor(0, pixels.Color(0,0,150));
-    pixels.show();
+    wsLED.setPixelColor(0, pixels.Color(0,0,150));
+    wsLED.show();
     delay(delayval);  
 }
