ffa4a2117cc4b9d5b43cd60b343c56a74b58dba4
Honza S. Copak <bonum.comunae@volny.cz>
2018-09-22 08:43:19 +0200

    Add files via upload

---
+++ esp32-micropython/02-ws1.py
@@ -0,0 +1,33 @@
+#octopusLAB - ESP32 - WS RGB LED (Neopixel)
+
+from time import sleep
+from machine import Pin
+from neopixel import NeoPixel
+
+BUILT_IN_LED = 2
+PIN_WS = 13
+POCET_LED = 1
+pin= Pin(PIN_WS, Pin.OUT)
+np = NeoPixel(pin, POCET_LED)
+
+pin_led = Pin(BUILT_IN_LED, Pin.OUT)
+
+def simple_blink():
+    pin_led.value(0)
+    sleep(1/2)
+    pin_led.value(1)
+    sleep(1/2)
+
+while True:
+    np[0] = (128, 0, 0)
+    np.write()
+    simple_blink() 
+    
+    np[0] = (0,128, 0)
+    np.write()
+    simple_blink() 
+    
+    np[0] = (0, 0, 128)
+    np.write()
+    simple_blink() 
+    
