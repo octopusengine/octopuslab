f698867847e6ede1994520387c6fca78acdd6e71
Honza S. Copak <bonum.comunae@volny.cz>
2018-09-22 18:54:22 +0200

    Update 05-serial-display.py

--- esp32-micropython/05-serial-display.py
+++ esp32-micropython/05-serial-display.py
@@ -1,7 +1,7 @@
-from pyb import UART
+from machine import UART
 
-uart = UART(1, 9600)
-uart.init(9600, bits=8, parity=None, stop=1, timeout=1000)
+uart = UART(0, 9600)
+#uart.init(9600, bits=8, parity=None, stop=1, timeout=1000)
 
 uart.write('C')    #clear
 uart.write('W7')   #change color
