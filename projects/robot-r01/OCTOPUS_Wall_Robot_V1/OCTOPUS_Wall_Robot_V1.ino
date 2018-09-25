ff1ff022be444c235a04ef71516353fa6d8e153c
Matěj Suchánek <kkartk322@gmail.com>
2018-09-25 21:38:03 +0200

    Final V1.1

--- projects/robot-r01/OCTOPUS_Wall_Robot_V1/OCTOPUS_Wall_Robot_V1.ino
+++ projects/robot-r01/OCTOPUS_Wall_Robot_V1/OCTOPUS_Wall_Robot_V1.ino
@@ -3,15 +3,15 @@
 
 #include "octopusLAB_ROBOT.h"
 
-#define CYCLES_PER_SECOND 100
+#define CYCLES_PER_SECOND 200
 
 int speed_forward = 90;
 int speed_pidcorrection = 90;
 
 float kK = 1;
 float pK = 0.75;
-float dK = 120;
-float iK = 0;
+float dK = 100;
+float iK = 0; //Nechat na 0 - potřebuje dodělat
 int n_EMA = 10;
 
 void setup() {
@@ -19,6 +19,7 @@ void setup() {
   initRobot();
 }
 
+long loop_millis_last = 0;
 float error_last = 0;
 
 void loop() {
@@ -27,30 +28,41 @@ void loop() {
   waitForButtonPress();
   wsSetGreen();
   delay(500);
-  
+
   while (!isButtonPressed()) {
+
+    long microseconds = micros();
+
+    while (millis() - loop_millis_last < 1000 / CYCLES_PER_SECOND) {
+      delayMicroseconds(100);
+    }
+
+    loop_millis_last = millis();
+
     int echo = updateEcho();
     int laser = getLaserDistance();
-
-    Serial.print("Btn: " + String(isButtonPressed()));
-
-    Serial.print(", Ech: " + String(echo));
+    
+    Serial.print("Ech: " + String(echo));
     Serial.print(", Las: " + String(laser));
 
+    //Serial.print(", Btn: " + String(isButtonPressed()));
+   
     float error = calcLaserError(laser);
     int delta = calcSpeedCorrection(error, error_last);
     error_last = error;
 
-    Serial.print(", Err: " + String(error));
+    //Serial.print(", Err: " + String(error));
 
     int speed_left = speed_forward + delta;
     int speed_right = speed_forward - delta;
 
-    Serial.print(", Left: " + String(speed_left));
-    Serial.print(", Rght: " + String(speed_right));
+    //Serial.print(", Left: " + String(speed_left));
+    //Serial.print(", Rght: " + String(speed_right));
 
     motorLeft(speed_left);
     motorRight(speed_right);
+
+    Serial.println(", uS: " + String(micros() - microseconds));
 
     Serial.println();
   }
@@ -58,6 +70,6 @@ void loop() {
   motorLeft(0);
   motorRight(0);
   while (isButtonPressed())
-  delay(10);
+    delay(10);
 
 }
