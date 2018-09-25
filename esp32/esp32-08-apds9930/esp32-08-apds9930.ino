5b28aea752669186a0b5a771555b0aea2b7d9dd5
Honza S. Copak <bonum.comunae@volny.cz>
2018-09-25 15:03:15 +0200

    Add files via upload

---
+++ esp32/esp32-08-apds9930/esp32-08-apds9930.ino
@@ -0,0 +1,24 @@
+//esp32-08-apds9930 - simple test: one distance senzor
+//octopusLAB - ESP32 ROBOT board (2018/09)
+
+#define DEBUG
+
+#include "octopusLAB_ROBOT.h"
+//#define PIN_WS  13
+
+#include <APDS9930.h>
+APDS9930 apds = APDS9930();
+
+void setup() {
+  apds.init(); // APDS-9930
+  apds.enableProximitySensor(false); // enable proximity, no interrupt
+
+  Serial.begin(57600);
+}
+
+void loop() {
+  while (true) {
+    Serial.println((String("Vzdalenost: ") + String(apds.readProximity())));
+    delay(50);
+  }
+}
