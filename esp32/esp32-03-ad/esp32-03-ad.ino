b8f5b4ffe4cb4223ebe07cb287a6bc6bea776aab
Petr Kracík <petrkr@petrkr.net>
2018-10-02 14:11:51 +0200

    ESP32: AD test

--- esp32/esp32-03-ad/esp32-03-ad.ino
+++ esp32/esp32-03-ad/esp32-03-ad.ino
@@ -1,21 +1,20 @@
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+// For graphs you can use plotter function in arduino
+
+int VINMES = 36;    // RobotBoard v2
+int sensorValue = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+
+  Serial.println("Init");
 }
 
 void loop() {
   // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);
-  // stop the program for <sensorValue> milliseconds:
-  delay(sensorValue);
-  // turn the ledPin off:
-  digitalWrite(ledPin, LOW);
-  // stop the program for for <sensorValue> milliseconds:
-  delay(sensorValue);
+  sensorValue = analogRead(VINMES);
+
+  Serial.print("Analog: ");
+  Serial.println(sensorValue);
+  delay(50);
 }
