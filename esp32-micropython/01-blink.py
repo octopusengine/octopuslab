c7503c848646eb7268ae9b8beed284ed7f89790c
Honza S. Copak <bonum.comunae@volny.cz>
2018-10-20 10:34:21 +0200

    edit ampy linux and win comPort arguments

--- esp32-micropython/01-blink.py
+++ esp32-micropython/01-blink.py
@@ -2,7 +2,12 @@
 This is first basic test ESP32 programming
 needs only ESP32 (DoIt 2x15pin)
 Installation:
+Linux:
+ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py 
 ampy -p /dev/ttyUSB0 put ./01-blink.py main.py
+Windows:
+ampy -p /COM6 put ./octopus_robot_board.py
+ampy -p /COM6 put ./01-blink.py main.py
 # reset device
 """
 from machine import Pin
