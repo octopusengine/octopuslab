d10b255690f77451c95b342cece72c0493234bef
Honza S. Copak <bonum.comunae@volny.cz>
2018-10-20 10:44:53 +0200

    edit octopus library

--- esp32-micropython/01-blink.py
+++ esp32-micropython/01-blink.py
@@ -1,20 +1,24 @@
 """
 This is first basic test ESP32 programming
 needs only ESP32 (DoIt 2x15pin)
+
 Installation:
 Linux:
-ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py 
+ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py
 ampy -p /dev/ttyUSB0 put ./01-blink.py main.py
-Windows:
+Windows: (COM6 or another detected comPort)
 ampy -p /COM6 put ./octopus_robot_board.py
 ampy -p /COM6 put ./01-blink.py main.py
 # reset device
 """
 from machine import Pin
 from time import sleep
-pin_diody = Pin(2, Pin.OUT)
+
+import octopus_robot_board as o #octopusLab main library - "o" < octopus
+
+led = Pin(o.BUILT_IN_LED, Pin.OUT)
 while True:
-    pin_diody.value(0)
+    led.value(0)
     sleep(1/2)
-    pin_diody.value(1)
+    led.value(1)
     sleep(1/2)
