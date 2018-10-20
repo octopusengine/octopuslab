2a3f380fa40014bed519d5778888fdf3890b51b9
Honza S. Copak <bonum.comunae@volny.cz>
2018-10-20 11:22:41 +0200

    setup i2c over octopus library

--- esp32-micropython/05-oled.py
+++ esp32-micropython/05-oled.py
@@ -5,6 +5,7 @@ needs external driver ssd1306.py from https://github.com/micropython/micropython
 Set your SCL and SDA pins in constants
 
 Installation:
+ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py
 ampy -p /dev/ttyUSB0 put ./ssd1306.py
 ampy -p /dev/ttyUSB0 put ./05-oled.py main.py
 # reset device
@@ -14,10 +15,9 @@ import machine
 import ssd1306
 import time
 
-I2C_SCL_PIN = 22
-I2C_SDA_PIN = 21
+import octopus_robot_board as o #octopusLab main library - "o" < octopus
 
-i2c = machine.I2C(-1, machine.Pin(I2C_SCL_PIN), machine.Pin(I2C_SDA_PIN))
+i2c = machine.I2C(-1, machine.Pin(o.I2C_SCL_PIN), machine.Pin(o.I2C_SDA_PIN))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # test_whole display
@@ -31,5 +31,13 @@ oled.fill(0)
 oled.show()
 
 # write text on x, y
-oled.text('OctopusLab', 20, 20)
+oled.text('OLED test', 20, 20)
 oled.show()
+time.sleep_ms(2000)
+
+for i in range(1,10+1):
+   oled.fill(0)
+   oled.text('OctopusLab', 20, 15)
+   oled.text(" --- "+str(10-i)+" ---", 20, 35)
+   oled.show()
+   time.sleep_ms(1000)
