20ea579b388328d705914af33867dc774d1aafc4
Honza S. Copak <bonum.comunae@volny.cz>
2018-10-20 11:35:51 +0200

    edit pins and comment

--- esp32-micropython/octopus_robot_board.py
+++ esp32-micropython/octopus_robot_board.py
@@ -1,11 +1,21 @@
+"""
+This is octopusLab basic library for robotBoard PCB
+Edition: --- 20.10.2018 ---
+
+Installation:
+ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py
+"""
+
 #ESP32 pinout setup
 BUILT_IN_LED = 2
-WS_LED_PIN = 13
+WS_LED_PIN = 13    #ws RGB ledi diode
+ONE_WIRE_PIN = 32  #one wire (for Dallas temperature sensor)
+
 #I2C:
 I2C_SCL_PIN = 22
 I2C_SDA_PIN = 21
-#one wire for Dallas temperature sensor
-ONE_WIRE_PIN = 32
+
+#---/
 
 
 
