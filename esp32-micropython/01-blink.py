b3ffa8742245617de5dc8bb0369ff3bc41759991
Honza S. Copak <bonum.comunae@volny.cz>
2018-10-20 10:26:51 +0200

    Update 01-blink.py

--- esp32-micropython/01-blink.py
+++ esp32-micropython/01-blink.py
@@ -1,3 +1,10 @@
+"""
+This is first basic test ESP32 programming
+needs only ESP32 (DoIt 2x15pin)
+Installation:
+ampy -p /dev/ttyUSB0 put ./01-blink.py main.py
+# reset device
+"""
 from machine import Pin
 from time import sleep
 pin_diody = Pin(2, Pin.OUT)
