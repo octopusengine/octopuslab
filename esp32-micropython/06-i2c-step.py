694cf6cda8a7d2077dd8ce88d9c8e31e3ae7cd8c
Vašek Chalupníček <vasekch@users.noreply.github.com>
2018-10-26 14:31:34 +0200

    added step motor library and example usage

---
+++ esp32-micropython/06-i2c-step.py
@@ -0,0 +1,31 @@
+"""
+This is simple usage of 28BYJ-48 step motor on ULN2803 driver via PFC8574 I2C expander
+
+Set your expander address and motor id in constants
+
+Installation:
+ampy -p /dev/ttyUSB0 put ./sm28byj48.py
+ampy -p /COM6 put ./06-i2c-step.py main.py
+# reset device
+
+"""
+from machine import Pin, I2C
+from sm28byj48 import SM28BYJ48
+
+import octopus_robot_board as o #octopusLab main library - "o" < octopus
+
+#PCF address = 35 #33-0x21/35-0x23
+ADDRESS = 0x23
+# motor id 1 or 2
+MOTOR_ID = 1
+
+i2c_sda = Pin(o.I2C_SDA_PIN, Pin.IN,  Pin.PULL_UP)
+i2c_scl = Pin(o.I2C_SCL_PIN, Pin.OUT, Pin.PULL_UP)
+
+i2c = I2C(scl=i2c_scl, sda=i2c_sda, freq=100000) # 100kHz as Expander is slow :(
+motor1 = SM28BYJ48(i2c, ADDRESS, MOTOR_ID)
+
+# turn right 90 deg
+motor1.turn_degree(90)
+# turn left 90 deg
+motor1.turn_degree(90, 1)
--- esp32-micropython/octopus_robot_board.py
+++ esp32-micropython/octopus_robot_board.py
@@ -6,26 +6,26 @@ Installation:
 ampy -p /dev/ttyUSB0 put ./octopus_robot_board.py
 """
 
+from micropython import const
+
 #ESP32 pinout setup
-BUILT_IN_LED = 2
-WS_LED_PIN = 13    #ws RGB ledi diode
-ONE_WIRE_PIN = 32  #one wire (for Dallas temperature sensor)
+BUILT_IN_LED = const(2)
+WS_LED_PIN = const(13)    #ws RGB ledi diode
+ONE_WIRE_PIN = const(32)  #one wire (for Dallas temperature sensor)
 
 #I2C:
-I2C_SCL_PIN = 22
-I2C_SDA_PIN = 21
+I2C_SCL_PIN = const(22)
+I2C_SDA_PIN = const(21)
 
 #PWM/servo:
-PWM1_PIN = 17
-PWM2_PIN = 16
-PWM3_PIN = 4
+PWM1_PIN = const(17)
+PWM2_PIN = const(16)
+PWM3_PIN = const(4)
 
 #inputs:
-I39_PIN = 39
-I34_PIN = 34
-I35_PIN = 35
+I39_PIN = const(39)
+I34_PIN = const(34)
+I35_PIN = const(35)
 
 #main analog input (for power management)
-PIN_ANALOG = 36
-
-#---/
+PIN_ANALOG = const(36)
