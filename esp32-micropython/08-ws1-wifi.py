9ed6ad221ae9bb916e0b7f16ed2baf1cdb708818
Petr Kracík <petrkr@petrkr.net>
2018-11-06 17:35:47 +0100

    ESP32Micropython: wsled wifi demo

--- esp32-micropython/08-ws1-wifi.py
+++ esp32-micropython/08-ws1-wifi.py
@@ -4,32 +4,56 @@ from time import sleep
 from machine import Pin
 from neopixel import NeoPixel
 
-import WIFIconnect as WIFIC
+# Include wifi
+from WIFIconnect import WiFiConnect
 
-BUILT_IN_LED = 2
-PIN_WS = 13
-POCET_LED = 1
-pin= Pin(PIN_WS, Pin.OUT)
-np = NeoPixel(pin, POCET_LED)
+# Remeber set SSID and password in WiFiConfig.py file
+import WiFiConfig
 
-pin_led = Pin(BUILT_IN_LED, Pin.OUT)
+# Import OctopusLab Robot board definition file
+import octopus_robot_board as o # octopusLab main library - "o" < octopus
+
+# Warning: RobotBoard version 1 use different WS Pin 13 !
+pin_ws = Pin(13, Pin.OUT)
+
+# Robot Board v2 (and newer)
+# pin_ws = Pin(o.WS_LED_PIN, Pin.OUT)
+
+np = NeoPixel(pin_ws, 1)
+
+pin_led = Pin(o.BUILT_IN_LED, Pin.OUT)
+
 
 def simple_blink():
     pin_led.value(0)
-    sleep(1/2)
+    sleep(0.1)
     pin_led.value(1)
-    sleep(1/2)
+    sleep(0.1)
 
+
+# Default WS led light RED as init
 np[0] = (128, 0, 0)
 np.write()
 simple_blink()
 
-WIFIC.do_connect()
 
-np[0] = (0,128, 0)
-np.write()
-simple_blink()
+# Define function callback for connecting event
+def connected_callback(sta):
+    simple_blink()
+    np[0] = (0, 128, 0)
+    np.write()
+    simple_blink()
+    print(sta.ifconfig())
 
-np[0] = (0, 0, 128)
-np.write()
-simple_blink()
+
+def connecting_callback():
+    np[0] = (0, 0, 128)
+    np.write()
+    simple_blink()
+
+
+w = WiFiConnect()
+w.events_add_connecting(connecting_callback)
+w.events_add_connected(connected_callback)
+
+w.connect(WiFiConfig.WIFI_SSID, WiFiConfig.WIFI_PASS)
