9c90323ea8c7741c508861c67106d94c6fcc8f5c
Petr Kracík <petrkr@petrkr.net>
2018-11-06 16:24:05 +0100

    ESP32Micropython: WIFI connect, change TAB to spaces, wait to connect

--- esp32-micropython/WIFIconnect.py
+++ esp32-micropython/WIFIconnect.py
@@ -1,35 +1,37 @@
 # include this in boot.py or main.py as WIFIC
-# and call WIFIC.do_connect() from the boot.py or main.py
+# and call WIFIconnect.do_connect() from the boot.py or main.py
 
 # Don't forget to change the ssid and password under second comment
 
-def do_connect():
-	# import network functions
-	import network
-	
-	# set AP name and password
-	ssid = "ssid"
-	password = "password"
+# Includes
+import network
+from time import sleep
 
-	# get an instance of the sta_if WiFi interface
-	sta_if = network.WLAN(network.STA_IF)
 
-	# check if we are already connected to a WiFi
-	if sta_if.isconnected() == True:
-		print("Already connected")
-		return
+def do_connect(ssid, password):
+    # get an instance of the sta_if WiFi interface
+    sta_if = network.WLAN(network.STA_IF)
 
-	# activate interface
-	sta_if.active(True)
-	
-	# connect to network via provided ID
-	print('connecting to network...')
-	sta_if.connect(ssid, password)
+    # check if we are already connected to a WiFi
+    if sta_if.isconnected():
+        print("Already connected")
+        return
 
-	# print connection info - automatic
-	# currently this prints out as if no connection was established - giving 0.0.0.0 sd ip
-	# however, connection IS made and functional
-	print('network config:', sta_if.ifconfig())
-	
-	# simple newline to separate from base information during boot
-	print('')
+        # activate interface
+    sta_if.active(True)
+
+    # connect to network via provided ID
+    print('connecting to "{}" network...'.format(ssid))
+    sta_if.connect(ssid, password)
+
+    while not sta_if.isconnected():
+        print("  -- Waiting to connect")
+        sleep(0.5)
+
+    # print connection info - automatic
+    # currently this prints out as if no connection was established - giving 0.0.0.0 sd ip
+    # however, connection IS made and functional
+    print('network config:', sta_if.ifconfig())
+
+    # simple newline to separate from base information during boot
+    print('')
