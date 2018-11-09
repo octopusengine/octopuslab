50a73bfe5b16653b26d0a7d99c800df2c680aced
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-09 22:32:32 +0100

    Update 04-hall.py

--- esp32-micropython/04-hall.py
+++ esp32-micropython/04-hall.py
@@ -1,8 +1,4 @@
-// esp32-04-hall - simple test: magnetic field detection
-
-from machine import Pin
-from time import sleep
-
+#esp32-04-hall - simple test: magnetic field detection
 #hallRead()
 
 """
