69837e9f6a33d452e6dda0cfdf177e92cbe86abb
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-10 10:48:09 +0100

    refactoring - lib.temperature

--- esp32-micropython/09-temperature-oled.py
+++ esp32-micropython/09-temperature-oled.py
@@ -1,14 +1,13 @@
 """
 This example usage of DS18B20 "Dallas" temperature sensor and SSD1306 OLED display
 needs external driver ssd1306.py from https://github.com/micropython/micropython/blob/master/drivers/display/ssd1306.py
-needs external module temperature.py from ??
+needs external module temperature.py from https://boneskull.com/micropython-on-esp32-part-1/
 
-Set your SCL and SDA pins in constants
 Set your sensor pin number in constants
 
 Installation:
-ampy -p /dev/ttyUSB0 put ./ssd1306.py
-ampy -p /dev/ttyUSB0 put ./temperature.py
+lib/ssd1306.py
+lib/temperature.py
 ampy -p /dev/ttyUSB0 put ./09-temperature-oled.py main.py
 # reset device
 """
@@ -17,25 +16,20 @@ import machine
 import ssd1306
 import time
 
-from temperature import TemperatureSensor
+from lib.temperature import TemperatureSensor
+import octopus_robot_board as o # octopus ROBOTboard pinout
 
-ONE_WIRE_PIN = 32
-I2C_SCL_PIN = 22
-I2C_SDA_PIN = 21
+ts = TemperatureSensor(o.ONE_WIRE_PIN)
 
-ts = TemperatureSensor(ONE_WIRE_PIN)
-
-i2c = machine.I2C(-1, machine.Pin(I2C_SCL_PIN), machine.Pin(I2C_SDA_PIN))
+i2c = machine.I2C(-1, machine.Pin(o.I2C_SCL_PIN), machine.Pin(o.I2C_SDA_PIN))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # test_whole display
 oled.fill(1)
 oled.show()
-
 time.sleep_ms(300)
 
-# reset display
-oled.fill(0)
+oled.fill(0) # reset display
 oled.show()
 
 while True:
@@ -44,6 +38,7 @@ while True:
     print(temp)
     # print on display
     oled.fill(0) # reset display
-    oled.text('T = %.1f C' % temp, 25, 20)
+    oled.text('octopus LAB', 20, 20)
+    oled.text('Temp: %.1f C' % temp, 20, 35)
     oled.show()
-    time.sleep_ms(500)
+    time.sleep_ms(1000)
