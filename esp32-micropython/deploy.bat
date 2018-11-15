56a44391d97137e98a6a83d0a6e2887c9a8b80ff
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-15 15:05:21 +0100

    Update deploy.bat

--- esp32-micropython/deploy.bat
+++ esp32-micropython/deploy.bat
@@ -8,9 +8,12 @@ ampy -p /COM6 mkdir lib
 ampy -p /COM6 put ./lib/ssd1306.py lib/ssd1306.py
 ampy -p /COM6 put ./lib/temperature.py lib/temperature.py
 
+ampy -p /COM6 mkdir config
+
 ampy -p /COM6 mkdir util
 ampy -p /COM6 put ./util/setup.py util/setup.py
 ampy -p /COM6 put ./util/octopus.py util/octopus.py
 ampy -p /COM6 ls util
+
 echo start: 
 echo >>> octopus() 
