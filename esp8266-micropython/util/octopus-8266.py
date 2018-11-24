36ecc2784f14c1b5dadaa7af5643ca17fc608dad
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-24 18:22:08 +0100

    temp octopus() for esp8266

--- esp8266-micropython/util/octopus-8266.py
+++ esp8266-micropython/util/octopus-8266.py
@@ -1,11 +1,14 @@
-# this module is to setup your board throuhg REPL
-# it's loaded in boot.py and provides function setup()
+# this module is for Basic simple examples & tests
+# it's loaded in boot.py and provides function octopus()
 # user is questioned in interactive mode
 
+# esp8266 / wemos / esp32 doit...
+
 # ampy -p /COM4 put util/octopus-8266.py util/octopus.py
+
 import time
 import os, uos
-import ujson, json
+import json, ujson
 import gc #mem_free
 import machine, ubinascii
 from machine import Pin, PWM, Timer
@@ -51,17 +54,18 @@ def get_eui():
 def printLine():
     print("------------------------------------------------")
 
-def menu():
+def mainMenu():
     printLine()
-    print("Menu: 1 setup WiFi | 2 basic test | 3 system info")
+    print("Menu: Basic simple examples & tests")
     printLine()
-    print("B - built-in led test")
-    print("O - oled display test")
-    print("R - RGB WS led test")
-    print("S - setup machine and wifi")
-    print("T - temperature")
-    print("W - wifi test")
-    print("Q - QUIT")
+    print("[b] - built-in led/beep/button")
+    print("[i] - device & system info")
+    print("[o] - oled display test")
+    print("[r] - RGB WS led test")
+    print("[s] - setup machine and wifi")
+    print("[t] - temperature")
+    print("[w] - wifi test")
+    print("[q] - QUIT")
     printLine()
 
     sel = input("select: ")
@@ -70,9 +74,10 @@ def menu():
 
 #-------------
 def octopus():
-    beep(pwm0,500,100)
-    tim.init(period=1000, mode=Timer.ONE_SHOT, callback=lambda t:print("test timer - delay"))
+    beep(pwm0,500,100) # start beep
+    tim.init(period=1000, mode=Timer.ONE_SHOT, callback=lambda t:print("test timer - thread delay"))
     #tim.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+
     print("      ,'''`.")
     print("     /      \ ")
     print("     |(@)(@)|")
@@ -82,7 +87,7 @@ def octopus():
     print("   )  \ `)(' / ( ")
     print()
     print("Hello, this is basic octopusLAB example (2018/11)")
-    print("(Press Ctrl+C to abort)")
+    print("(Press Ctrl+C to abort | CTRL+D to soft reboot)")
     print()
 
     time.sleep_us(10)       # sleep for 10 microseconds
@@ -92,14 +97,19 @@ def octopus():
 
     run= True
     while run:
-      sel = menu()
+      sel = mainMenu()
       beep(pwm0,2000,50)
 
-      if sel == "1":
+      if sel == "b":
+           count = 5
+           for _ in range(count):
+               beep(pwm0,500,100)
+               blink(500)
+
+      if sel == "s":
           print(">>> setup()")
-      elif sel == "2":
-          print("TODO)")
-      elif sel == "3":
+
+      elif sel == "i":
           print("uPy version: "+str(os.uname()[3]))
           print("> unique_id: "+str(get_eui()))
           #print("--- MAC: "+str(mac2eui(get_eui())))
@@ -107,7 +117,7 @@ def octopus():
           print("> mem_free: "+str(gc.mem_free()))
           print("> machine.freq: "+str(machine.freq()))
 
-      if sel == "O":
+      if sel == "o":
           from lib import ssd1306
           i2c = machine.I2C(-1, machine.Pin(I2C_SCL_PIN), machine.Pin(I2C_SDA_PIN))
           oled = ssd1306.SSD1306_I2C(128, 64, i2c)
@@ -125,7 +135,11 @@ def octopus():
           oled.show()
           time.sleep_ms(1000)
 
-      if sel == "R":
+      if sel == "s":
+          from util.setup import setup
+          setup()
+
+      if sel == "r":
         from neopixel import NeoPixel
         NUMBER_LED = 1
         pin = Pin(WS_LED_PIN, Pin.OUT)
@@ -146,17 +160,8 @@ def octopus():
         np[0] = (0, 0, 0) #0
         np.write()
 
-      if sel == "W":
-          from util.wifi_connect import WiFiConnect
-          f = open('config/wifi.json', 'r')
-          d = f.read()
-          f.close()
-          j = json.loads(d)
-          ssid=j["wifi_ssid"]
-          print("config for: " + ssid)
-          w = WiFiConnect()
-          w.connect(ssid,j["wifi_pass"])
-          print("WiFi: OK")
+      if sel == "q":
+          run = False
 
     delta = time.ticks_diff(time.ticks_ms(), start) # compute time difference
     print("> delta time: "+str(delta))
