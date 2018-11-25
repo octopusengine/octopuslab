306d88c38c6b826c99bf12fbab2e8736bd7081ac
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-25 16:46:42 +0100

    com port as variable

--- esp32-micropython/deploy.bat
+++ esp32-micropython/deploy.bat
@@ -1,19 +1,31 @@
-echo Welcome to basic octopusLAB script!
-echo (edit for your COM port)
+echo Welcome to basic octopusLAB script for ESP32 - Micropython!
 pause
-ampy -p /COM6 put boot.py
-ampy -p /COM6 put ./octopus_robot_board.py
 
-ampy -p /COM6 mkdir lib
-ampy -p /COM6 put ./lib/ssd1306.py lib/ssd1306.py
-ampy -p /COM6 put ./lib/temperature.py lib/temperature.py
+REM Setup your COM port
+SET PORT=/COM7
+your port is: %PORT%
 
-ampy -p /COM6 mkdir config
+REM To skip the following commands, put "REM" before them:
 
-ampy -p /COM6 mkdir util
-ampy -p /COM6 put ./util/setup.py util/setup.py
-ampy -p /COM6 put ./util/octopus.py util/octopus.py
-ampy -p /COM6 ls util
+ampy -p  %PORT% put boot.py
+ampy -p  %PORT% put ./octopus_robot_board.py
 
-echo start: 
-echo >>> octopus() 
+ampy -p  %PORT% mkdir lib
+ampy -p  %PORT% put ./lib/ssd1306.py lib/ssd1306.py
+ampy -p  %PORT% put ./lib/temperature.py lib/temperature.py
+ampy -p  %PORT% put ./lib/microWebSrv.py lib/microWebSrv.py
+ampy -p  %PORT% put ./lib/microWebSocket.py  lib/microWebSocket.py
+ampy -p  %PORT% put ./lib/sm28byj48.py lib/sm28byj48.py
+
+ampy -p  %PORT% mkdir wwwesp
+ampy -p  %PORT% put ./wwwesp/index.html wwwesp/index.html
+ampy -p  %PORT% put ./wwwesp/octopus-logo100.png wwwesp/octopus-logo100.png
+
+ampy -p  %PORT% mkdir config
+ampy -p  %PORT% mkdir util
+ampy -p  %PORT% put ./util/setup.py util/setup.py
+ampy -p  %PORT% put ./util/octopus.py util/octopus.py
+ampy -p  %PORT% put ./util/wifi_connect.py util/wifi_connect.py
+ampy -p  %PORT% ls util
+echo start:
+echo >>> octopus()
