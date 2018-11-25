9979ba2c54a8bdccc85946650c888eb59b6598bb
Honza S. Copak <bonum.comunae@volny.cz>
2018-11-25 17:23:00 +0100

    add lib/max7219_8digit.py

--- esp8266-micropython/deploy8266.bat
+++ esp8266-micropython/deploy8266.bat
@@ -1,22 +1,28 @@
 echo Welcome to basic octopusLAB script!
 pause
-SET PORT=/COM1
+
+REM Setup your COM port
+SET PORT=/COM7
 your port is: %PORT%
+
+REM To skip the following commands, put "REM" before them:
 
 ampy -p  %PORT% put boot.py
 ampy -p  %PORT% put ./octopus_robot_board.py
 
 ampy -p  %PORT% mkdir lib
 ampy -p  %PORT% put ./lib/ssd1306.py lib/ssd1306.py
+ampy -p  %PORT% put ./lib/max7219_8digit.py lib/max7219_8digit.py
 ampy -p  %PORT% put ./lib/temperature.py lib/temperature.py
 ampy -p  %PORT% put ./lib/microWebSrv.py lib/microWebSrv.py
 ampy -p  %PORT% put ./lib/microWebSocket.py  lib/microWebSocket.py
-ampy -p  %PORT% put ./lib/sm28byj48.py lib/sm28byj48.py
+REM ampy -p  %PORT% put ./lib/sm28byj48.py lib/sm28byj48.py
 
 ampy -p  %PORT% mkdir wwwesp
 ampy -p  %PORT% put ./wwwesp/index.html wwwesp/index.html
 ampy -p  %PORT% put ./wwwesp/octopus-logo100.png wwwesp/octopus-logo100.png
 
+ampy -p  %PORT% mkdir config
 ampy -p  %PORT% mkdir util
 ampy -p  %PORT% put ./util/setup.py util/setup.py
 ampy -p  %PORT% put ./util/octopus-8266.py util/octopus.py
