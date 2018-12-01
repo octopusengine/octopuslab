bd0fe213357f7bf6bf823e8932801d1d13ad4e1b
Honza S. Copak <bonum.comunae@volny.cz>
2018-12-01 17:49:56 +0100

    add boards width pinouts select

--- esp32-micropython/util/setup.py
+++ esp32-micropython/util/setup.py
@@ -6,42 +6,67 @@
 import uos
 import ujson
 
+devices = [
+["oLAB RobotBoard1","esp32"],
+["oLAB IoTBoard1","esp8266"],
+["oLAB IoTBoard1","esp32"]
+]
+
 def setup():
-    print("Hello this will help you initialize your ESP")
+    print("Hello, this will help you initialize your ESP")
     print("Press Ctrl+C to abort")
     print()
+    print('=' * 30)
+    print("[d] - device setting")
+    print("[w] - wifi")
+    print("[q] - QUIT")
+    print('=' * 30)
+    sel = input("select: ")
 
     # TODO improve this
     # prepare directory
     if 'config' not in uos.listdir():
-        uos.makedirs('config')
+       uos.makedirs('config')
 
-    print("Step 1/2 - Device setting:")
-    print()
-    dc = {}
-    dc['board_type'] = input("Board type ('oLAB RobotBoard1' or 'oLAB IoTBoard1'): ")
-    dc['soc_type'] = input("SoC type ('esp32' or 'esp8266'): ")
+    if sel == "d":
+        print("Device setting:")
+        print("   board_type  | soc_type (system on the board)")
+        i=0
+        for di in devices:
+           print(str(i)+": "+str(di[0]) + " | " + str(di[1]))
+           i=i+1
 
-    print("Writing to file config/device.json")
-    with open('config/device.json', 'w') as f:
-        ujson.dump(dc, f)
-        # ujson.dump(wc, f, ensure_ascii=False, indent=4)
-    print()
+        print()
+        sd = input("select: ")
+        #print(str(devices[int(sd)]))
+        print("> " + str(devices[int(sd)][0]) + " | " + str(devices[int(sd)][1]))
 
-    print("Step 2/2 - WiFi setting:")
-    print()
-    wc = {}
-    wc['wifi_ssid'] = input("SSID: ")
-    wc['wifi_pass'] = input("PASSWORD: ")
 
-    # TODO improve this
-    if 'config' not in uos.listdir():
-        uos.makedirs('config')
+        dc = {}
+        dc['board_type'] = str(devices[int(sd)][0]) #input("Board type ('oLAB RobotBoard1' or 'oLAB IoTBoard1'): ")
+        dc['soc_type'] = str(devices[int(sd)][1])   #input("SoC type ('esp32' or 'esp8266'): ")
 
-    print("Writing to file config/wifi.json")
-    with open('config/wifi.json', 'w') as f:
-        ujson.dump(wc, f)
-        # ujson.dump(wc, f, ensure_ascii=False, indent=4)
-    print()
-    
+        print("Writing to file config/device.json")
+        with open('config/device.json', 'w') as f:
+            ujson.dump(dc, f)
+            # ujson.dump(wc, f, ensure_ascii=False, indent=4)
+        print()
+
+    if sel == "w":
+        print("WiFi setting:")
+        print()
+        wc = {}
+        wc['wifi_ssid'] = input("SSID: ")
+        wc['wifi_pass'] = input("PASSWORD: ")
+
+        # TODO improve this
+        if 'config' not in uos.listdir():
+            uos.makedirs('config')
+
+        print("Writing to file config/wifi.json")
+        with open('config/wifi.json', 'w') as f:
+            ujson.dump(wc, f)
+            # ujson.dump(wc, f, ensure_ascii=False, indent=4)
+        print()
+
     print("all OK, press CTRL+D to soft reboot")
