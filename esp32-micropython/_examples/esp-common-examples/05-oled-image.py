a49749b54da27777dc51c2edf94f09a30ab7c6cb
Honza S. Copak <bonum.comunae@volny.cz>
2018-12-15 19:42:05 +0100

    fix > pinout

--- esp32-micropython/_examples/esp-common-examples/05-oled-image.py
+++ esp32-micropython/_examples/esp-common-examples/05-oled-image.py
@@ -13,22 +13,24 @@ Set your image WIDTH and HEIGHT in constants
 Installation:
 octopus_robot_board.py
 lib/ssd1306.py lib/ssd1306.py
-assets/octopus_image.pbm 
+assets/octopus_image.pbm
 ampy -p /dev/ttyUSB0 put ./05-oled-image.py main.py
 # reset device
 """
-
 import framebuf
 import machine
 import time
 from lib import ssd1306
+from util.pinout import set_pinout
+pinout = set_pinout()
 
-import octopus_robot_board as o #octopusLab main library - "o" < octopus
+i2c_sda = machine.Pin(pinout.I2C_SDA_PIN, machine.Pin.IN,  machine.Pin.PULL_UP)
+i2c_scl = machine.Pin(pinout.I2C_SCL_PIN, machine.Pin.OUT, machine.Pin.PULL_UP)
 
 IMAGE_WIDTH = 63
 IMAGE_HEIGHT = 63
 
-i2c = machine.I2C(-1, machine.Pin(o.I2C_SCL_PIN), machine.Pin(o.I2C_SDA_PIN))
+i2c = machine.I2C(-1, i2c_scl, i2c_sda)
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # test_whole display
@@ -40,7 +42,6 @@ time.sleep_ms(300)
 # reset display
 oled.fill(0)
 oled.show()
-
 
 with open('assets/octopus_image.pbm', 'rb') as f:
     f.readline() # Magic number
