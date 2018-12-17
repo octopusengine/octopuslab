ec415b0ba3a178cbf945b1abf7cc0607c080cf06
KominekJan <kominek.jan@hotmail.com>
2018-12-17 19:23:29 +0100

    Add files via upload
    Upravený TouchPin.py

---
+++ esp32-micropython/_examples/esp32-iot-examples/TouchCheck.py
@@ -0,0 +1,27 @@
+from machine import Pin,TouchPad
+from time import sleep
+t1 = TouchPad(Pin(4))
+t2 = TouchPad(Pin(33))
+
+def assignPins(pin1, pin2)
+    t1 = pin1
+    t2 = pin2
+    
+def readButton(threshold, delay):
+    b1 = 0.0
+    b2 = 0.0
+
+    while True:
+        if t1.read() < threshold: 
+            b1 = b1 + delay
+        elif b1 != 0.0:
+            print("Pin4 pressed for %f" % b1)
+            b1 = 0.0
+        
+        if t2.read() < threshold: 
+            b2 += delay
+        elif b2 != 0.0:
+            print("Pin33 pressed for %f" % b2)
+            b2 = 0.0
+            
+        sleep(delay)
