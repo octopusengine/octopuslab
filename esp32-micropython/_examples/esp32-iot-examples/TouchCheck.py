fbd1be7dea4a14b038b8a37a1d18a7b31005e5be
KominekJan <kominek.jan@hotmail.com>
2018-12-17 19:28:56 +0100

    Update TouchCheck.py
    typo

--- esp32-micropython/_examples/esp32-iot-examples/TouchCheck.py
+++ esp32-micropython/_examples/esp32-iot-examples/TouchCheck.py
@@ -4,8 +4,8 @@ t1 = TouchPad(Pin(4))
 t2 = TouchPad(Pin(33))
 
 def assignPins(pin1, pin2)
-    t1 = pin1
-    t2 = pin2
+    t1 = TouchPad(Pin(pin1))
+    t2 = TouchPad(Pin(pin2))
     
 def readButton(threshold, delay):
     b1 = 0.0
@@ -15,13 +15,13 @@ def readButton(threshold, delay):
         if t1.read() < threshold: 
             b1 = b1 + delay
         elif b1 != 0.0:
-            print("Pin4 pressed for %f" % b1)
+            print("Pin{0} pressed for {1}".format(pin1, b1))
             b1 = 0.0
         
         if t2.read() < threshold: 
             b2 += delay
         elif b2 != 0.0:
-            print("Pin33 pressed for %f" % b2)
+            print("Pin{0} pressed for {1}".format(pin2, b2))
             b2 = 0.0
             
         sleep(delay)
