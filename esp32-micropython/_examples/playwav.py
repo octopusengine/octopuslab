f159433de160de1fcb6d7b0530f7b16a8a635b5b
Petr Kracík <petrkr@petrkr.net>
2018-12-18 18:12:28 +0100

    Read WAV header and determine size and sample rate

--- esp32-micropython/_examples/playwav.py
+++ esp32-micropython/_examples/playwav.py
@@ -2,6 +2,8 @@
 
 import os
 
+import struct
+
 # Timer
 from machine import Timer
 
@@ -61,14 +63,42 @@ dw = dac.write
 wav_pos = 0
 wav_pos_prev = 0
 
-# Determine size of WAV
-wav_file = "11__koala-11k.raw"
-wav_size = os.stat(wav_file)[6]
+wav_file = "11__koala-11k.wav"
+wav_file = "20__holandska.wav"
+wav_file = "22__dzn dzn dzn-11k.wav"
+wav_file = "untitled3.wav"
+
 wav = open(wav_file, "rb")
 wr = wav.read
+
+# Read WAV header
+wav.seek(20)
+wav_format, \
+wav_channels, \
+wav_sample_rate, \
+wav_byterate, \
+wav_blockalign, \
+wav_bits_per_sample, \
+wav_datasignature, \
+wav_data_size = struct.unpack('HHIIHH4sI', wav.read(24))
+
+wav_size = wav_data_size + 44
+
+# Seek to PCM data begin
+wav_pos = wav.seek(44)
+
+print("WAV File: {0}\n\
+WAV Format: {1}\n\
+WAV Channels: {2}\n\
+WAV Sample rate: {3}\n\
+WAV Bits per sample: {4}".format(wav_file,
+                                 wav_format,
+                                 wav_channels,
+                                 wav_sample_rate,
+                                 wav_bits_per_sample))
 
 t1 = Timer(0)
 t2 = Timer(1)
 
-t1.init(freq=11200, mode=Timer.PERIODIC, callback=play)
+t1.init(freq=wav_sample_rate, mode=Timer.PERIODIC, callback=play)
 t2.init(freq=1, mode=Timer.PERIODIC, callback=actualSampleRate)
