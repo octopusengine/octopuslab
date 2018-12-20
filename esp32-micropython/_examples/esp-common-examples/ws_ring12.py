3ec7da1bbd28dd3e8dad5a20cf047b546c8c2b5c
Honza S. Copak <bonum.comunae@volny.cz>
2018-12-20 20:53:08 +0100

    fix c

--- esp32-micropython/_examples/esp-common-examples/ws_ring12.py
+++ esp32-micropython/_examples/esp-common-examples/ws_ring12.py
@@ -89,34 +89,3 @@ while True:
     print("clear")
     clear()
     time.sleep_ms(2000)
-
-
-    """ ---- c ----
-    void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i*1+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-  else if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  else {
-    WheelPos -= 170;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-"""
