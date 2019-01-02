c63e4dd7eafea71a87330fa0ed78c270c20fffb1
Honza S. Copak <bonum.comunae@volny.cz>
2019-01-02 17:58:51 +0100

    add pins for moisture sensor

--- esp32-micropython/pinouts/olab_esp32_iot_board1.py
+++ esp32-micropython/pinouts/olab_esp32_iot_board1.py
@@ -22,18 +22,22 @@ I2C_SDA_PIN = const(21)
 PIEZZO_PIN = const(27)  #14 //MOTOR_4A = const(27)
 WS_LED_PIN = const(15)  #TDO
 ANALOG_PIN = const(36)
-ONE_WIRE_PIN = const(32) # = DEV1_PIN
+ONE_WIRE_PIN = const(32)
 RELAY_PIN = const(33)
 MFET_PIN = const(14)
 
-BUTT1_PIN = const(25)
-BUTT2_PIN = const(12) #TDI
-BUTT3_PIN = const(13) #TCK
+BUTT1_PIN = const(25) # up
+BUTT2_PIN = const(12) # o #TDI
+BUTT3_PIN = const(13) # dw #TCK
 
-PWM1_PIN = const(17) #PWM/servo
+PWM1_PIN = const(17) #PWM/servo //pwr for moisture
 PWM2_PIN = const(16)
 PWM3_PIN = const(4)
 
 DEV1_PIN = const(32)
 DEV2_PIN = const(26)
-#---/
+
+#inputs:
+I39_PIN = const(39)
+I34_PIN = const(34)
+I35_PIN = const(35) #analog in for moisture
