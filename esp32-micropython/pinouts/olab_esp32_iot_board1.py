8cede5ea1eacbd511fbba1f7ac86f6d2639b92c2
Vašek Chalupníček <vasekch@users.noreply.github.com>
2019-05-10 15:48:55 +0200

    refactored pinouts to an inheritance model

--- esp32-micropython/pinouts/olab_esp32_iot_board1.py
+++ esp32-micropython/pinouts/olab_esp32_iot_board1.py
@@ -4,19 +4,7 @@ I2C / SPI / MOTORs / SERVO / PWM...
 Edition: --- 2.1.2019 ---
 """
 from micropython import const
-# PIN as on octopusLAB Wemos ESP32 IoTBoard1
-BUILT_IN_LED = const(2)
-HALL_SENSOR = const(8)
-
-#SPI:
-SPI_CLK_PIN  = const(18)
-SPI_MISO_PIN = const(19)
-SPI_MOSI_PIN = const(23)
-SPI_CS0_PIN  = const(5)
-
-#I2C:
-I2C_SCL_PIN = const(22)
-I2C_SDA_PIN = const(21)
+from pinouts.olab_esp32_base import *
 
 #---esp32---IoT board:
 PIEZZO_PIN = const(27)  #14 //MOTOR_4A = const(27)
