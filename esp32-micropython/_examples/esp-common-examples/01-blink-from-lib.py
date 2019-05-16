6cb09eebe69bfe0a5cd92aea848f31dc8ed8a572
Honza S. Copak <bonum.comunae@volny.cz>
2019-05-16 13:55:44 +0200

    add new example

---
+++ esp32-micropython/_examples/esp-common-examples/01-blink-from-lib.py
@@ -0,0 +1,25 @@
+"""
+This is first basic test ESP8266/32 programming
+blink with BUILT_IN_LED
+"""
+from time import sleep
+from machine import Pin
+
+from util.pinout import set_pinout
+pinout = set_pinout()
+
+pin_led = Pin(pinout.BUILT_IN_LED, Pin.OUT)
+
+print("simple demo:")
+# esp_id = ubinascii.hexlify(machine.unique_id()).decode()                 
+# print(esp_id)
+
+def simple_blink():
+    pin_led.value(1)
+    sleep(0.5)
+    pin_led.value(0)
+    sleep(0.5)
+
+print("blinking led")
+while True:
+    simple_blink()
