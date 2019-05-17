eaaa9c58917fea73f9021764b74b10ebfc8f8624
Honza S. Copak <bonum.comunae@volny.cz>
2019-05-17 11:42:32 +0200

    add oled display

--- esp32-micropython/_examples/mqtt-examples/mqtt-slave.py
+++ esp32-micropython/_examples/mqtt-examples/mqtt-slave.py
@@ -1,8 +1,7 @@
 #octopusLAB - ESP32 - WiFi and WS RGB LED signalizationn - MQTT
 #
 
-import machine
-import ubinascii
+import machine, time, ubinascii
 from time import sleep
 from machine import Pin, Timer, PWM, SPI
 from neopixel import NeoPixel
@@ -17,17 +16,34 @@ from ds18x20 import DS18X20
 from util.pinout import set_pinout
 pinout = set_pinout()
 
-ver = "16.5.2019-v:0.2"
+printLog(1,"boot device >")
 print("mqtt-slave.py > ESP32")
+ver = "0.21/17.5.2019"
 print(ver)
 
+#* = todo / ## = automatic
+
 # Defaults - sensors
-isTemp = 1          # temperature
-isLight = 0         # light (lux)
-isMois = 0          # moisture
-isAD = 0            # AD input voltage
-isADL = 0           # AD photoresistor
-is7seg = 1          # max 8x7 segm.display
+isTemp = 0      #  temperature
+isLight = 0     #  light (lux)
+isMois = 0      #* moisture
+isAD = 0        #* AD input voltage
+isADL = 0       #  AD photoresistor
+# Displays
+isLed7 = 1      #  SPI max 8x7 segm.display
+isOLED = 1      ##  I2C
+isLCD = 0       ##* I2C
+isSD = 0        #* UART
+
+LCD_ADDRESS=0x27
+LCD_ROWS=2
+LCD_COLS=16
+
+#OLED size and position
+OLEDX = 128
+OLEDY = 64
+OLED_x0 = 3
+OLED_ydown = OLEDY-7
 
 # hard-code config / daefault
 Debug = True        # TODO: debugPrint()?
@@ -51,23 +67,50 @@ ws_r = 0
 ws_g = 0
 ws_b = 0
 
+printLog(2,"init - variables and functions >")
+
 bd = bytes.decode
 
-ts = []
-if isTemp:
-    print("init dallas temp >")
+# Set up I2C
+if Debug: print("init i2c >")
+i2c = machine.I2C(-1, machine.Pin(pinout.I2C_SCL_PIN), machine.Pin(pinout.I2C_SDA_PIN))
+# i2c = machine.I2C(-1, machine.Pin(pinout.I2C_SCL_PIN), machine.Pin(pinout.I2C_SDA_PIN), freq=100000) # 100kHz because PCF is slow
+if Debug: print(" - scanning")
+i2cdevs = i2c.scan()
+if Debug: print(" - devices: {0}".format(i2cdevs))
+
+# Determine what we have connected to I2C
+isOLED = 0x3c in i2cdevs
+bhLight = 0x23 in i2cdevs
+bh2Light = 0x5c in i2cdevs
+tslLight = 0x39 in i2cdevs
+
+oled = 0
+def oled_intit():
+    print("OLED present: {0}".format(isOLED))
+    global oled
+    from lib import ssd1306
+    sleep(1)
+    oled = ssd1306.SSD1306_I2C(OLEDX, OLEDY, i2c)
+
+def displMessage2(mess,timm): # OLEDdisplMessage()
+    #TODO: OLED/TFT/LCD...
     try:
-        ds = DS18X20(OneWire(dspin))
-        ts = ds.scan()
+        oled.fill_rect(0,OLED_ydown,OLEDX,10,0)
+        oled.text(mess, OLED_x0, OLED_ydown)
+        oled.show()
+        time.sleep_ms(timm*1000)
+    except Exception as e:
+       print("Err. displMessage() Exception: {0}".format(e)) 
 
-        if len(ts) <= 0:
-            isTemp = False
-
-        for t in ts:
-            print(" --{0}".format(bytearrayToHexString(t)))
-    except:
-        isTemp = False
-    print("Found {0} dallas sensors, temp active: {1}".format(len(ts), isTemp))
+def displMessage(mess,timm):
+    try:
+        oled.fill_rect(0,OLED_ydown-17,OLEDX,10,0)
+        oled.text(mess, OLED_x0, OLED_ydown-17)
+        oled.show()
+        time.sleep_ms(timm*1000)
+    except Exception as e:
+       print("Err. displMessage2() Exception: {0}".format(e))              
 
 def getTemp():
     tw=999
@@ -77,13 +120,14 @@ def getTemp():
         for t in ts:
             temp = ds.read_temp(t)
             tw = int(temp*10)
-    return tw        
+    return tw  
 
 def timerInit():
     tim1.init(period=10000, mode=Timer.PERIODIC, callback=lambda t:timerSend()) 
 
 def timeSetup():
     if Debug: print("time setup >")
+    urlApi ="http://www.octopusengine.org/api/hydrop/"
     urltime=urlApi+"/get-datetime.php"
     try:
         response = urequests.get(urltime)
@@ -94,19 +138,7 @@ def timeSetup():
         print(str(rtc.datetime()))
     except:
         print("Err. Setup time from WiFi")    
-
-if is7seg:
-    from lib.max7219_8digit import Display
-    # spi
-    try:
-        #spi.deinit()
-        #print("spi > close")
-        spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(pinout.SPI_CLK_PIN), mosi=Pin(pinout.SPI_MOSI_PIN))
-        ss = Pin(pinout.SPI_CS0_PIN, Pin.OUT)
-        d7 = Display(spi, ss)
-    except:
-        print("spi.ERR")
-
+    
 def simple_blink():
     pin_led.value(0)
     sleep(0.1)
@@ -145,12 +177,19 @@ it = 0 # every 10 sec.
 def timerSend():
     global it
     it = it+1
-    if Debug: print(">"+str(it))
+    if Debug: print("timer > "+str(it))
 
-    if is7seg:
+    """
+    if isLed7:
         if isTemp:
             d7.write_to_buffer(str(it)+"-"+str(int(getTemp())))
-            d7.display()    
+            d7.display()
+        else:
+            d7.write_to_buffer(str(it))
+            d7.display()        
+    """
+    if isOLED:
+        displMessage2(str(it) + " | " + str(get_hhmm(rtc)),1)    
 
     if (it == 6*minute): # 6 = 1min / 60 = 10min
         if Debug: print("10 min. > send data:")
@@ -164,7 +203,6 @@ def connected_callback(sta):
     #np.write()
     simple_blink()
     print(sta.ifconfig())
-
 
 def connecting_callback(retries):
     #np[0] = (0, 0, 128)
@@ -234,7 +272,74 @@ def mqtt_sub(topic, msg):
             d7.write_to_buffer(data)
             d7.display() 
         except:
-               print("mqtt.8x7seg.ERR")     
+            print("mqtt.8x7seg.ERR") 
+
+    if "oled1" in topic:
+        data = bd(msg)  
+        try:
+            displMessage(data,2)
+        except:
+            print("oled.ERR") 
+
+    if "oled2" in topic:
+        data = bd(msg)  
+        try:
+            displMessage2(data,2)
+        except:
+            print("oled.ERR")                   
+
+# --- init ---
+printLog(3,"init i/o - config >")
+ts = []
+if isTemp:
+    print("dallas temp >")
+    try:
+        ds = DS18X20(OneWire(dspin))
+        ts = ds.scan()
+
+        if len(ts) <= 0:
+            isTemp = False
+
+        for t in ts:
+            print(" --{0}".format(bytearrayToHexString(t)))
+    except:
+        isTemp = False
+    print("Found {0} dallas sensors, temp active: {1}".format(len(ts), isTemp))
+
+if isLed7:
+    from lib.max7219_8digit import Display
+    # spi
+    try:
+        #spi.deinit()
+        #print("spi > close")
+        spi = SPI(1, baudrate=10000000, polarity=1, phase=0, sck=Pin(pinout.SPI_CLK_PIN), mosi=Pin(pinout.SPI_MOSI_PIN))
+        ss = Pin(pinout.SPI_CS0_PIN, Pin.OUT)
+        d7 = Display(spi, ss)
+    except:
+        print("spi.ERR")
+
+if not 0x27 in i2c.scan():
+    print("I2C LCD display not found!")
+    isLCD = False
+    #raise Exception("No device")
+
+if isLCD:
+    from lib.esp8266_i2c_lcd import I2cLcd
+    lcd = I2cLcd(i2c, LCD_ADDRESS, LCD_ROWS, LCD_COLS)
+
+# serial displ
+if isSD:
+    from machine import UART
+    uart = UART(2, 9600) #UART2 > #U2TXD(SERVO1/PWM1_PIN)
+    uart.write('C')      #test quick clear display  
+
+if isOLED:
+    oled_intit()
+    oled.text('MQTT-SLAVE test', 5, 5)
+    # oled.text(get_hhmm(), 45,29) #time HH:MM
+    oled.hline(0,50,128,1)
+    oled.text("octopusLAB 2019",5,OLED_ydown) 
+    oled.show()    
                 
 # Default WS led light RED as init
 np[0] = (100, 0, 0)
@@ -244,9 +349,10 @@ simple_blink()
 np[0] = (0, 0, 0)
 np.write()        
 
-print("init i/o >")
-if is7seg:
+if isLed7:
     test7seg() 
+
+printLog(4,"wifi and mqtt >")    
 
 print("wifi_config >")
 wifi_config = read_wifi_config()
@@ -281,11 +387,21 @@ print("mqtt log")
 # mqtt_root_topic_temp = "octopus/device"
 c.publish(mqtt_root_topic,esp_id) # topic, message (value) to publish
 
-# timeSetup()
-timerInit()
-
 print("test temp: " + str(getTemp()))
 
-print("> loop:")
+"""
+timeSetup()
+if isOLED:
+    oled.text(get_hhmm(), 45,29) #time HH:   
+    oled.show() 
+if isLed7:
+    d7.write_to_buffer(get_hhmm())
+    d7.display() 
+"""
+
+timerInit()
+print(get_hhmm(rtc))
+
+printLog(5,"start - main loop >")
 while True:
     c.check_msg()
