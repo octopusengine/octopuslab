7d97c2462e0444b8dc5189ceba7eedbf010a710c
Petr Kracík <petrkr@petrkr.net>
2019-07-13 19:55:42 +0900

    LED: Support None pin

--- esp32-micropython/util/led/__init__.py
+++ esp32-micropython/util/led/__init__.py
@@ -1,6 +1,9 @@
 from time import sleep_ms
 
 def blink(pin_obj, length_on=1000, length_off=1000):
+    if pin_obj is None:
+        return
+
     if length_off == 1000 and length_on != 1000:
         length_off = length_on
     pin_obj.value(1)
