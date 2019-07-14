cfd8dd1a30487ac30d1790570021ad4e268a5b20
Petr Kracík <petrkr@petrkr.net>
2019-07-14 11:28:36 +0900

    LED: Created class

--- esp32-micropython/util/led/__init__.py
+++ esp32-micropython/util/led/__init__.py
@@ -1,4 +1,5 @@
 from time import sleep_ms
+from machine import Pin
 
 def blink(pin, length_on=1000, length_off=1000):
     print("WARGNING: DEPRECATED: Do not USE, Use Led class instead!!")
@@ -15,8 +16,41 @@ def blink(pin, length_on=1000, length_off=1000):
     pin.value(0)
     sleep_ms(length_off)
 
-def ledvalue(pin_obj, value):
-    if pin_obj is None:
-        return
+class Led:
+    def __init__(self, pin, value=False):
+        self.pin = None
+        self.state = value
 
-    pin_obj.value(value)
+        if pin is None:
+            print("WARN: Pin is None, this led will be dummy")
+            return
+
+        self.pin = Pin(pin, Pin.OUT)
+        self.pin.value(self.state)
+
+    def blink(self, length_on=1000, length_off=1000):
+        if length_off == 1000 and length_on != 1000:
+            length_off = length_on
+
+        self.toggle()
+        sleep_ms(length_on)
+        self.toggle()
+        sleep_ms(length_off)
+
+    def value(self, value=None):
+        if value is None:
+            if self.pin is not None:
+                self.state = self.pin.value() # Update internal value in case someone changed it from outside
+
+            return self.state
+
+        self.state = value
+
+        if self.pin is None:
+            print("DUMMY_LED Value {0}".format(self.state))
+            return
+
+        self.pin.value(self.state)
+
+    def toggle(self):
+        self.value(not self.value())
