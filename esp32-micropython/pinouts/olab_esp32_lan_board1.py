29fa1425856cb719b5702fd33e705dc2e5197bf2
Petr Kracík <petrkr@petrkr.net>
2019-07-16 10:06:01 +0200

    Pinout: LAN board, added UART 1 pins

--- esp32-micropython/pinouts/olab_esp32_lan_board1.py
+++ esp32-micropython/pinouts/olab_esp32_lan_board1.py
@@ -10,6 +10,8 @@ from pinouts.olab_esp32_base import *
 BUILT_IN_LED = const(4)
 HALL_SENSOR = const(8)
 
+PIEZZO_PIN = const(15)
+
 #I2C:
 I2C_SCL_PIN = const(16)
 I2C_SDA_PIN = const(2)
@@ -21,3 +23,7 @@ SPI_MOSI_PIN = const(33)
 SPI_CS0_PIN  = const(5)
 
 BUTT1_PIN = const(0) # up
+
+# UART 1
+RXD1 = const(36)
+TXD1 = const(4)
