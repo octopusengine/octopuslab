eff307bc0c68f4f0c7f848ccc2ed0c853d7e3eca
Honza S. Copak <bonum.comunae@volny.cz>
2019-07-16 14:29:21 +0200

    only class

--- esp32-micropython/util/buzzer/__init__.py
+++ esp32-micropython/util/buzzer/__init__.py
@@ -1,49 +1,7 @@
 from time import sleep_ms
 from machine import Pin, PWM
 
-def play_tone(pwm_pin, freq, length, volume=50):
-    print("WARGNING: DEPRECATED: Do not USE, Use Buzzer class instead!!")
-
-    if pwm_pin is None:
-        print("DUMMY_BUZZ DEPRECATED: play tone (freq {0}, time {1} volume {2})".format(freq, length, volume))
-        sleep_ms(length)
-        return
-
-    # continuously
-    pwm_pin.duty(volume)
-    pwm_pin.freq(freq)
-    sleep_ms(length)
-
-def beep(p,f,t):  # port,freq,time
-    print("WARGNING: DEPRECATED: Do not USE, Use Buzzer class instead!!")
-
-    if p is None:
-        print("DUMMY_BUZZ DEPRECATED: beep (freq {0}, time {1})".format(f ,t))
-        sleep_ms(t)
-        return
-
-    #pwm0.freq()  # get current frequency
-    p.freq(f)     # set frequency
-    #pwm0.duty()  # get current duty cycle
-    p.duty(512)   # set duty cycle
-    sleep_ms(t)
-    p.duty(0)
-    #b.deinit()
-
-def play_melody(pwm_pin, melody, volume=50):
-    print("WARGNING: DEPRECATED: Do not USE, Use Buzzer class instead!!")
-
-    if pwm_pin is None:
-        print("DUMMY_BUZZ DEPRECATED: play melody")
-        return
-
-    for note in melody:
-        if note == 0:
-            pwm_pin.duty(0)
-        else:
-            pwm_pin.duty(volume)
-            pwm_pin.freq(note)
-        sleep_ms(150)
+# old beep and play_tone functions? > Use Buzzer class instead!
 
 class Buzzer():
     def __init__(self, pin):
