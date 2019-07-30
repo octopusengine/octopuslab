fdd57ec390bf2e5b97f47b967b060766b0dfb21b
Honza S. Copak <bonum.comunae@volny.cz>
2019-07-30 18:18:33 +0200

    adding setup time from server

--- esp32-micropython/examples/clock.py
+++ esp32-micropython/examples/clock.py
@@ -1,12 +1,17 @@
+# simple basic example - ESP32 + 7segment display
+
 from util.octopus import *
 
-d = disp7_init()   
+w()			# wifi connect
+d = disp7_init()	# 8 x 7segment display init   
 
 def clock():
 	disp7(d,get_hhmm("-"))
 	sleep(0.5)
 	disp7(d,get_hhmm(" "))        
 	sleep(0.5)
+
+timeSetup() 		# server > time setup
 
 while True:
 	clock()
