09e49c083d665a418c859ab61ba4394061fad76e
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-02 15:59:12 +0200

    delete colors

--- esp32-micropython/util/rgb/__init__.py
+++ esp32-micropython/util/rgb/__init__.py
@@ -4,16 +4,6 @@ from time import sleep, sleep_ms
 from machine import Pin
 from neopixel import NeoPixel
 
-# WS neopixel:
-RED = (255, 0, 0)
-ORANGE = (255, 64, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-BLACK = (0, 0, 0) # = off
-
 class Rgb(NeoPixel):
     def __init__(self, pin, num=1):
         self.pin = pin
