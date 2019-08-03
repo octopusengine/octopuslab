4b5201194270d2ab83d59b103f07e8555fa464d7
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-03 13:46:57 +0200

    added oled-x, oled-y parameters

--- esp32-micropython/util/oled/__init__.py
+++ esp32-micropython/util/oled/__init__.py
@@ -11,15 +11,13 @@ OLED_ydown = OLEDY-7
 
 class Oled(ssd1306.SSD1306_I2C):
     def __init__(self, i2c, ox=OLEDX, oy=OLEDY):
-        print("__init__()")
-        print(ox)
-        print(oy)
-        sleep_ms(500)
-        super().__init__(128, 64, i2c)
+        self.ox = ox
+        self.oy = oy
+        super().__init__(ox, oy, i2c)
 
     def test(self):
         self.text('oled display OK', OLED_x0, 3)
-        self.hline(0,52,128,1)
+        self.hline(0,52,self.ox,1)
         self.text("octopusLAB 2019",OLED_x0,OLED_ydown)
         self.show()
 
@@ -28,4 +26,3 @@ class Oled(ssd1306.SSD1306_I2C):
             for x, c in enumerate(row):
                 self.pixel(x+posx, y+posy, c)
         self.show()
-       
