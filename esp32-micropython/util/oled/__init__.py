e3377d261973cdd4013744c32a8a3ac4ba27489a
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-03 14:23:30 +0200

    added draw_icon, draw_image

--- esp32-micropython/util/oled/__init__.py
+++ esp32-micropython/util/oled/__init__.py
@@ -3,6 +3,7 @@
 from time import sleep_ms
 from machine import Pin
 from lib import ssd1306
+from util.display_segment import *
 
 OLEDX = 128
 OLEDY = 64
@@ -26,3 +27,32 @@ class Oled(ssd1306.SSD1306_I2C):
             for x, c in enumerate(row):
                 self.pixel(x+posx, y+posy, c)
         self.show()
+
+    def draw_image(self, file="assets/octopus_image.pbm"):
+        import framebuf
+        IMAGE_WIDTH = 63
+        IMAGE_HEIGHT = 63
+
+        with open(file, 'rb') as f:
+            f.readline() # Magic number
+            f.readline() # Creator comment
+            f.readline() # Dimensions
+            data = bytearray(f.read())
+            fbuf = framebuf.FrameBuffer(data, IMAGE_WIDTH, IMAGE_HEIGHT, framebuf.MONO_HLSB)
+            # To display just blit it to the display's framebuffer (note you need to invert, since ON pixels are dark on a normal screen, light on OLED).
+            self.invert(1)
+            self.blit(fbuf, 0, 0)
+        self.show()    
+
+    def oledSegment(self,num,point=False,deg=False):
+        threeDigits(num,point,deg)    
+
+"""
+def oledSegmentTest(oled):
+    print("oled segment test >")
+    oled.fill(0)
+    oled.text('octopusLAB test', OLED_x0, 3)
+    for num in range(100):
+        oledSegment(oled,100-num)
+        sleep_ms(50)
+"""                
