d5fc17db3868246053d6c8a690e7c0da68b6e8ef
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-04 19:42:14 +0200

    Add files via upload

---
+++ esp32-micropython/examples/oled_random_lines.py
@@ -0,0 +1,22 @@
+# octopusLAB simple xexample
+# HW: ESP32 + i2c OLED display
+# ampy -p /COM6 put examples/oled_random_lines.py main.py
+
+octopus()            # include main library
+o = oled_init()      # init oled display
+o.fill(0)            # clear
+
+# default coordinates for position
+x = 0
+y = 0
+
+while True:   
+   old_x = x
+   old_y = y
+   # get new position
+   x = int(urandom(1)[0]/2)
+   y = int(urandom(1)[0]/4)
+
+   o.line(old_x, old_y, x, y, 1)
+   o.show()
+   # sleep(0.1) # slow or fast
