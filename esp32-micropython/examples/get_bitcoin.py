8f79f9664c18bdce0235ef6634f83f6322f06784
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-06 22:45:21 +0200

    btc-usd from coinmarketcup - example

---
+++ esp32-micropython/examples/get_bitcoin.py
@@ -0,0 +1,21 @@
+# get bitcoin
+
+from time import sleep
+import urequests, json
+from util.octopus import *
+
+w()
+d7 = disp7_init()
+
+def bitcoin_usd():
+    res = urequests.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
+    btcusd = res.json()[0]['price_usd']
+    return float(btcusd)
+
+while True:
+    btc = bitcoin_usd()
+    print(btc)
+    d7.show(btc)
+    sleep(20)
+    d7.show("btc-usd")
+    sleep(3)
