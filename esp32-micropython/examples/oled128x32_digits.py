e4621f82fd184ab923385dc2e7431b9809edc68d
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-10 15:33:09 +0200

    oled 128x32 - digit example

---
+++ esp32-micropython/examples/oled128x32_digits.py
@@ -0,0 +1,36 @@
+# octopusLAB simple example
+# HW: ESP32 + i2c small OLED display 128x32
+
+from time import sleep
+
+octopus()            # include main library
+o = oled_init(128, 32)      # init oled display
+from util.display_segment import displayDigit
+
+def displayNum(num, row = 0):
+    num = str(num)
+    for n in range(len(num)):
+        displayDigit(o, int(num[n]), n, row * 17)
+
+o.clear()
+for n in range(10):
+    displayDigit(o, n, n)
+
+for n in range(10):
+    displayDigit(o, 9-n, n, 17)
+
+sleep(3)
+displayNum(314256987)
+
+
+
+
+
+
+
+
+
+
+
+
+
