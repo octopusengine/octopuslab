e603b45e7dcec7b2d62330593bf8c4160bcb93f4
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-21 12:56:09 +0200

    class Analog - battery monitoring

---
+++ esp32-micropython/examples/oled_battery_monitoring.py
@@ -0,0 +1,61 @@
+# octopusLAB simple example 
+# ESP32board + OLED display -> analog 36, 39, ...
+
+from util.analog import Analog
+octopus()       # include main library
+
+anIn = Analog(36) #analog input
+
+def sendValue(value = 0,urlPOST = "http://www.octopusengine.org/iot17/add18.php"):
+    from urequests import post
+    header = {}
+    header["Content-Type"] = "application/x-www-form-urlencoded"
+    deviceID = Env.uID
+    place = "octoPy32"
+    # value =  int(float(Env.ver)*100)
+    try:
+        postdata_v = "device={0}&place={1}&value={2}&type={3}".format(deviceID, place, value,"batRaw")
+        res = post(urlPOST, data=postdata_v, headers=header)
+        sleep_ms(100)
+        print("sendValue.ok")
+    except:
+        print("E.sendValue")
+
+print('Start >')
+led.blink()
+o = oled_init()
+sleep(3)
+o.clear()
+o.text("octopusLAB 2019",10,10)
+o.show()
+
+led.value(1)
+w()
+led.value(0)
+
+sleep(3)
+o.poweroff()
+
+print('Battery monitoring')
+
+#sleep for 10 seconds (10000 milliseconds)
+#machine.deepsleep(10000)
+ii = 0
+while True:
+    valRaw = anIn.get_adc_aver()
+    valPrint = str(ii)+ ": "+str(valRaw)
+    print(valPrint)
+    
+    o.poweron()
+    o.clear()
+    o.text(valPrint,10,10)
+    o.show()
+    sleep(6)
+
+    if ii%100 == 0:
+        sendValue(valRaw)
+
+    ii += 1
+
+
+
