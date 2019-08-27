7462a1db9c156534e088aea347b2cc52a1bda8ca
Honza S. Copak <bonum.comunae@volny.cz>
2019-08-27 14:54:23 +0200

    interupt handler

---
+++ esp32-micropython/examples/button_irq.py
@@ -0,0 +1,27 @@
+# octopusLAB simple example
+# HW: ESP32board + burtton 0 (boot) interupt
+
+from util.octopus import *
+from machine import Pin
+
+counter=0
+
+def irq_handler(v):
+    global value, counter
+    counter += 1
+    print("IRQ ", counter)
+    #led.toggle()
+
+    led.value(value)
+    if(value == 0):
+        value = 1
+    else:
+        value = 0
+
+value = 0
+
+button0 = Pin(0, Pin.IN)
+button0.irq(trigger=Pin.IRQ_FALLING, handler=irq_handler)
+
+while True:
+    pass
