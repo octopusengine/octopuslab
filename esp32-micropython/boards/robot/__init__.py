873134d185c673eb28619866483336a23d4ad88c
Honza S. Copak <bonum.comunae@volny.cz>
2019-09-01 11:18:57 +0200

    initial class files

---
+++ esp32-micropython/boards/robot/__init__.py
@@ -0,0 +1,12 @@
+# basic class for octopusLAB ROBOTboard
+# p r e p a r e
+
+
+class Robot:
+    def __init__(self, info="Temp"):
+        self.info = info        
+
+    def get_info(self):
+        return(self.info)
+
+    
