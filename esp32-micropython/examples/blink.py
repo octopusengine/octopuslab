a28a9db6684ed7f5656994d0bfb893ed2e36fe65
Honza S. Copak <bonum.comunae@volny.cz>
2019-09-27 08:56:38 +0200

    added comment

--- esp32-micropython/examples/blink.py
+++ esp32-micropython/examples/blink.py
@@ -3,12 +3,14 @@
 # run: import examples.blink
 
 from util.led import Led
+# from util.octopus import led # short way
 from util.pinout import set_pinout
 
 pinout = set_pinout()           # set board pinout
 led = Led(pinout.BUILT_IN_LED)  # BUILT_IN_LED = 2
 
 print("---examples/blink.py---")
+# start main loop
 
 while True:
     led.blink()
