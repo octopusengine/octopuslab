03cda6f76f44ab6139fea4ec8fb0bd609559f4ef
Honza S. Copak <bonum.comunae@volny.cz>
2019-09-27 08:53:01 +0200

    fix pinout

--- esp32-micropython/examples/blink.py
+++ esp32-micropython/examples/blink.py
@@ -1,15 +1,14 @@
 # octopusLAB simple example
 # ESP32board with "BUILT_IN_LED"
-# import examples.blink
+# run: import examples.blink
 
-# from util.octopus import octopus, button_init, button, led
-from util.octopus import *
-octopus()       # include main library
+from util.led import Led
+from util.pinout import set_pinout
+
+pinout = set_pinout()           # set board pinout
+led = Led(pinout.BUILT_IN_LED)  # BUILT_IN_LED = 2
 
 print("---examples/blink.py---")
 
-#def run():
 while True:
     led.blink()
-
-#run()
