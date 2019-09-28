3148e02723b5778eb9e95e2bc0ce3a6c0d528f7f
Honza S. Copak <bonum.comunae@volny.cz>
2019-09-28 08:58:30 +0200

    typo "line"

--- esp32-micropython/examples/empty.py
+++ esp32-micropython/examples/empty.py
@@ -2,6 +2,7 @@
 
 print("--- empty.py ---> main.py --- test")
 print("This is simple Micropython example | ESP32 & octopusLAB")
+print('-' * 50)
 from gc import mem_free
 print("--- RAM before octopus(): " + str(mem_free()))
 
