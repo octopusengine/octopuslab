a089b7c31bbff5fa6f00fe3bf40bc656382eac60
Petr Kracik <petrkr@petrkr.net>
2019-10-21 19:29:08 +0200

    Octopus: use class Pinout

--- esp32-micropython/util/octopus.py
+++ esp32-micropython/util/octopus.py
@@ -11,9 +11,19 @@ from sys import modules
 from time import sleep, sleep_ms, ticks_ms, ticks_diff
 from machine import Pin, Timer, RTC
 #from os import urandom
-from util.pinout import set_pinout
+from pinouts import Pinout
 from util.io_config import get_from_file
 
+def set_pinout():
+    try:
+        with open('config/device.json', 'r') as f:
+            d = f.read()
+            f.close()
+            device_config = json.loads(d)
+    except:
+        print("Device config 'config/device.json' does not exist, please run setup()")
+
+    return Pinout.getPinout(device_config.get('soc_type'), device_config.get('board_type'))
 
 # olab = Env()  # for initialized equipment
 pinout = set_pinout()  # set board pinout
