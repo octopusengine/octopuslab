f120f1e3945ca5a906a30ae47ec972401becbcc5
Petr Kracík <petrkr@petrkr.net>
2019-10-22 13:11:08 +0200

    pinout: added legacy pinout for backward compatibility

---
+++ esp32-micropython/util/pinout.py
@@ -0,0 +1,15 @@
+"""this module is to load proper pinout per config"""
+from pinouts import Pinout
+import json
+
+def set_pinout():
+    try:
+        import json
+        with open('config/device.json', 'r') as f:
+            d = f.read()
+            f.close()
+            device_config = json.loads(d)
+    except:
+        print("Device config 'config/device.json' does not exist, please run setup()")
+
+    return Pinout.getPinout(device_config.get('soc_type'), device_config.get('board_type'))
