1492111ddb7dc25ef52ca2ea4c9a4a0c4aadb3f8
Honza S. Copak <bonum.comunae@volny.cz>
2019-10-24 18:46:19 +0200

    added thermometer

--- esp32-micropython/util/iot/__init__.py
+++ esp32-micropython/util/iot/__init__.py
@@ -6,15 +6,25 @@ re1.value(1)
 
 pwm_led = Pwm()
 pwm_led.duty(300)
+
+from util.iot import TemperatureSensor
+tt = TemperatureSensor(32)
+tx = tt.ds.scan()
+tt.read_temp(tx[0])
+tt.read_temp(tx[0])
 """
 
 from time import sleep_ms
 from machine import Pin, PWM
 from util.pinout import set_pinout
+from onewire import OneWire
+from ds18x20 import DS18X20
+
 
 pinout = set_pinout()
 default_relay_pin = pinout.RELAY_PIN
-default_pwm_pin = pinout.MFET_PIN 
+default_pwm_pin = pinout.MFET_PIN
+# default_temp_pin = pinout.MFET_PIN
 
 class Relay:
     def __init__(self, pin=default_relay_pin, value=False):
@@ -75,3 +85,61 @@ class Pwm():
 
     def get_pin(self):
         return self.pin
+
+
+class Thermometer:
+    def __init__(self, pin=32):
+        from onewire import OneWire
+        from ds18x20 import DS18X20
+
+        self.pin = pin
+        self.ts = []
+        try:
+            self.ds = DS18X20(OneWire(Pin(self.pin)))
+            self.ts = self.ds.scan()
+        except Exception:
+            print("Found {0} dallas sensors, temp active: {1}".format(len(ts), io_conf.get('temp')))
+
+    def get_pin(self):
+        return self.pin
+
+    def get_temp(self, index):
+        self.ds.convert_temp()
+        sleep_ms(750)
+        temp = self.ds.read_temp(self.ts[index])
+        temp = int(temp * 10) / 10
+        return temp
+
+    def get_temp_n(self):
+        tw = []
+        self.ds.convert_temp()
+        sleep_ms(750)
+        for t in self.ts:
+            temp = self.ds.read_temp(t)
+            tw.append(int(temp * 10) / 10)
+        return tw
+
+"""
+class TemperatureSensor:
+    # Represents a Temperature sensor (Dallas DS18X20)
+    def __init__(self, pin):
+        # Finds address of single DS18B20 on bus specified by `pin`
+        self.ds = DS18X20(OneWire(Pin(pin)))
+        addrs = self.ds.scan()
+        if not addrs:
+            raise Exception('no DS18B20 found at bus on pin %d' % pin)
+        # save what should be the only address found
+        self.addr = addrs.pop()
+
+    def read_temp(self, addr, fahrenheit=False):
+        self.ds.convert_temp()
+        sleep_ms(750)
+        temp = self.ds.read_temp(addr)
+        if fahrenheit:
+            return self.c_to_f(temp)
+        return temp
+
+    @staticmethod
+    def c_to_f(c):
+        return (c * 1.8) + 32
+"""
