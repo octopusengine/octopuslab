b3fc4a8c6ccdfaeb0b734159419b55d774f13fb3
Honza S. Copak <bonum.comunae@volny.cz>
2019-10-25 18:50:15 +0200

    play_melody with tones lenght

--- esp32-micropython/util/buzzer/__init__.py
+++ esp32-micropython/util/buzzer/__init__.py
@@ -8,12 +8,18 @@ piezzo.beep()
 
 from time import sleep_ms
 from machine import Pin, PWM
-
+# from util.buzzer.notes import *
+# from util.buzzer.melody import alert1
+# piezzo.speed = 100 # BPM
+# piezzo.play_melody(alert1)
 # old beep and play_tone functions? > Use Buzzer class instead!
+
+
 
 class Buzzer():
     def __init__(self, pin):
         self.pwm = None
+        self.speed = 60 # BPM
 
         if pin is None:
             print("WARN: Pin is None, this buzzer will be dummy")
@@ -28,27 +34,44 @@ class Buzzer():
 
         self.pwm.duty(0)
 
-    def play_tone(self, freq, length, volume=50):
+
+    def play_tone(self, freq, length = 2, volume=50):
+        # length = 2 > 1/2, length = 8 > 1/8...
+        length_tone = int(60/self.speed*1000/length)
         if self.pwm is None:
             print("DUMMY_BUZZ: play tone (freq {0}, time {1} volume {2})".format(freq, length, volume))
-            sleep_ms(length)
+            sleep_ms(length_tone)
             return
 
         # continuously
         self.pwm.duty(volume)
         self.pwm.freq(freq)
-        sleep_ms(length)
+        sleep_ms(length_tone)
+        self.pwm.duty(0)
+
 
     def beep(self, freq=1000, length=100):  # port,freq,time / default 1000,100
         self.play_tone(freq, length, 512)
         self.nosound()
 
-    def play_melody(self, melody, volume=50):
+
+    def play_tones(self, melody, volume=50):
         for note in melody:
             if note == 0:
-                self.play_tone(0, 150, 0)
+                self.play_tone(0, 8, 0)
             else:
-                self.play_tone(note, 150, volume)
+                self.play_tone(note, 8, volume)
+
+        # Stop play in case in melody will not be zero on end
+        self.nosound()
+
+
+    def play_melody(self, melody, volume=50):
+        for note in melody:
+            if note[0] == 0:
+                self.play_tone(0, note[1], 0)
+            else:
+                self.play_tone(note[0], note[1], volume)
 
         # Stop play in case in melody will not be zero on end
         self.nosound()
