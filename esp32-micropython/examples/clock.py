092004b059a01ef68959bd790aa4c478b30220b3
Honza S. Copak <bonum.comunae@volny.cz>
2019-10-29 17:43:47 +0100

    new octopus lib

--- esp32-micropython/examples/clock.py
+++ esp32-micropython/examples/clock.py
@@ -1,19 +1,20 @@
 # simple basic example - ESP32 + 7segment display
 # cp("examples/clock.py") > main.py
 
+
 from time import sleep
 from util.octopus import w, disp7_init, get_hhmm, time_init, printTitle
 
-w()			# wifi connect
+w()	# wifi connect
 d7 = disp7_init()	# 8 x 7segment display init   
 
 def clock():
 	d7.show(get_hhmm("-"))
 	sleep(0.5)
-	d7.show(get_hhmm(" "))        
+	d7.show(get_hhmm(" "))
 	sleep(0.5)
 
-time_init() 		# server > time setup
+time_init() # server > time setup
 
 printTitle("examples/clock.py")
 print("this is simple Micropython example | ESP32 & octopusLAB")
