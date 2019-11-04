e9f2a4a201dc631f33117b5d47254c9ab0dae475
Honza S. Copak <bonum.comunae@volny.cz>
2019-11-04 14:21:48 +0100

    simple thermostat example

---
+++ esp32-micropython/examples/thermostat.py
@@ -0,0 +1,42 @@
+# octopusLAB example - 2019
+# simple dallas and relay thermostat
+
+# (temperature < temperature1) -> relay(on)
+# 
+from time import sleep
+from util.octopus import temp_init, disp7_init
+from util.iot import Relay
+
+print("setup > ")
+from config import Config 
+keys = ["temperature1","temperature2","relaypin"]
+config = Config("thermostat", keys)
+print("press ctl+C and config.setup()")
+sleep(3)
+
+print("init > ")
+d7 = disp7_init()	# 8 x 7segment display init
+t = temp_init()
+relay = Relay(config.config["relaypin"])
+sleep(2)
+
+print("start > ")
+config_temp = config.config["temperature1"]
+print("config temperature: " + str(config_temp))
+
+while True:
+    temp  = t.get_temp()
+    print(temp, config_temp)
+    d7.show(temp)
+    if temp < config_temp:
+        print("relayOn")
+        relay.value(1)
+        sleep(3)
+    else:
+        print("relayOff")
+        relay.value(0)
+        sleep(5)
+
+
+
+
