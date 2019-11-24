8e3ea77f570245426c6cd2fa583e13f624b33a16
Honza S. Copak <bonum.comunae@volny.cz>
2019-11-24 19:09:44 +0100

    gimbal two servos test

--- esp32-micropython/examples/pca9685.py
+++ esp32-micropython/examples/pca9685.py
@@ -1,9 +1,41 @@
+# octopusLAB - simple test for gimbal two servos
+
 from pca9685.servo import Servos
 from util.octopus import i2c_init
+from time import sleep_ms
+from util.transform import * # include all - only for example
+
+
+# blocking - simple test
+def sweep(s, start, stop, delay=5, step=1):
+   ang = start
+   servo.position(s, ang)
+   sleep_ms(delay)
+
+   if start < stop:
+      print("a")
+      while ang < stop:
+         ang = ang + step
+         servo.position(s, ang)
+         sleep_ms(delay)
+
+   if start > stop:
+      print("b")
+      while ang > stop:
+         ang = ang - step
+         servo.position(s, ang)
+         sleep_ms(delay)
+
 
 i2c = i2c_init(1)
 servo = Servos(i2c)
 
-servo.position(0,50)
-servo.position(1,50)
+def sweeptest():
+    sweep(0,30,160)
+    sweep(1,30,180)
+    sweep(0,160,30)
+    sweep(1,180,30)
+    
+sweeptest()
+
 
