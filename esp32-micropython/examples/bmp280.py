dc5be0b93e0996fe04adaa58599ab5372709f3f9
Honza S. Copak <bonum.comunae@volny.cz>
2019-12-11 15:44:06 +0100

    added init_

--- esp32-micropython/examples/bmp280.py
+++ esp32-micropython/examples/bmp280.py
@@ -1,12 +1,18 @@
-# simple example bmp280
+# simple example with sensor bmp280
+# import examples.bmp280
+# from examples.bmp280 import bmp_init
 
-from util.octopus import i2c_init
-# "-1" > SW for old lib:
-i2c = i2c_init(1,100000,-1)
+def bmp_init():
+   from util.octopus import i2c_init
+   # "-1" > SW for old lib:
+   i2c = i2c_init(1,100000,-1)
 
-from bmp280 import BMP280
+   from bmp280 import BMP280
 
-bmp = BMP280(i2c)
+   bmp = BMP280(i2c)
+   return bmp
+
+bmp = bmp_init()
 # bmp.oversample_sett = 2
 # bmp.baseline = 101325
 
