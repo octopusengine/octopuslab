a3cf8e538abc31fa7f9d5bc40b293a64dc53a379
Honza S. Copak <bonum.comunae@volny.cz>
2019-12-15 09:13:30 +0100

    bTree db - separate

--- esp32-micropython/util/database/__init__.py
+++ esp32-micropython/util/database/__init__.py
@@ -1,9 +1,6 @@
 """
 octopusLAB - database class
-from util.database import Db
-db = Db("test")
-db.addOne("one","1")
-db.listAll()
+
 """
 import btree
 from util.octopus import printTitle
@@ -15,30 +12,3 @@ class Database():
 
     def write(self, *args, **kwargs):
         raise NotImplementedError("Using abstract class")
-
-
-class BTreeDB(Database):
-    def __init__(self, name="octopus"):
-        self.file = name + ".db"
-        # simple test: root > todo sub directory
-
-        try:
-            f = open(self.file, "r+b")
-        except OSError:
-            f = open(self.file, "w+b")
-
-        self.data = btree.open(f)
-        #super().open(f)
-    
-    def addOne(self, key, val): # edit is the same > last value
-        self.data[key] = val
-        self.data.flush()
-
-    def delKey(self, key):
-        del self.data[key]
-    
-    def listAll(self):
-        printTitle("listAll > ")
-        for key in self.data:
-            print(key, end=" | ")
-            print(self.data[key])
