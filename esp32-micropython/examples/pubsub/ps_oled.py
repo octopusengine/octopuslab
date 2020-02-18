f7b734f1edf27ec60c0cef00d8ead656ed4541e2
Honza S. Copak <bonum.comunae@volny.cz>
2020-02-18 14:29:58 +0100

    pubsub oled test

--- esp32-micropython/examples/pubsub/ps_oled.py
+++ esp32-micropython/examples/pubsub/ps_oled.py
@@ -3,12 +3,29 @@
 # octopusLAB pubsub example
 
 from examples.pubsub.ps_init import pubsub
-from util.octopus import disp7_init
 
-print("start: ps_disp7.py")
-d7 = disp7_init()  # 8 x 7segment display init
+from time import sleep
+from util.octopus import oled_init
+from util.display_segment import fourDigits
 
-def display_num(value):
-    d7.show(value)
+o = oled_init()
 
-pubsub.subscribe('value', display_num)
+print("ps_oled:")
+print("this is simple Micropython example | ESP32 & octopusLAB")
+print()
+
+o.clear()
+o.contrast(10)
+o.hline(0,53,128,1)
+o.text("octopusLAB 2020", 3, 1)
+o.show()
+sleep(2)
+
+def display_show(text):
+    o.clear()
+    o.text("octopusLAB 2020", 3, 1)
+    o.hline(0,53,128,1)
+    o.text(str(text), 3, 55)
+    o.show()
+
+pubsub.subscribe('value', display_show)
