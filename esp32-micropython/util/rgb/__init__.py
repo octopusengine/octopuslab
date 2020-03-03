9e6d240a8d892dfb1f94cff9322b4ff3d22351f2
Honza S. Copak <bonum.comunae@volny.cz>
2020-03-03 17:51:54 +0100

    added intensity

--- esp32-micropython/util/rgb/__init__.py
+++ esp32-micropython/util/rgb/__init__.py
@@ -8,21 +8,25 @@ from neopixel import NeoPixel
 
 from util.colors import *
 
-def wheel(pos):
+
+def wheel(pos, dev = 1):
         # Input a value 0 to 255 to get a color value.
         # The colours are a transition r - g - b - back to r.
         if pos < 0 or pos > 255:
             return (0, 0, 0)
         if pos < 85:
-            return (255 - pos * 3, pos * 3, 0)
+            return (int((255 - pos * 3)/dev), int((pos * 3)/dev), 0)
         if pos < 170:
             pos -= 85
-            return (0, 255 - pos * 3, pos * 3)
+            return (0, int((255 - pos * 3)/dev), int((pos * 3)/dev))
         pos -= 170
-        return (pos * 3, 0, 255 - pos * 3)
+        return (int((pos * 3)/dev), 0,  int((255 - pos * 3)/dev))
+
 
 def random_color():
-    return wheel(urandom(1)[0])        
+    return wheel(urandom(1)[0])
+
+
 
 class Rgb(NeoPixel):
     def __init__(self, pin, num=1):
@@ -57,11 +61,11 @@ class Rgb(NeoPixel):
             self.np.write()
             sleep(wait)
 
-    def rainbow_cycle(self, wait=3):
+    def rainbow_cycle(self, wait=3, intensity = 2): 
         for j in range(255):
             for i in range(self.num):
                 rc_index = (i * 256 // self.num) + j
-                self.np[i] = wheel(rc_index & 255)
+                self.np[i] = wheel(rc_index & 255, dev = intensity)
             self.np.write()
             sleep_ms(wait)
 
