6a2ab449216fced8e05bea23461515fd156dff15
Honza S. Copak <bonum.comunae@volny.cz>
2020-03-07 12:56:53 +0100

    new library

--- esp32-micropython/examples/dcmotor_test.py
+++ esp32-micropython/examples/dcmotor_test.py
@@ -1,48 +1,23 @@
-# octopusLAB simple example
+# ROBOTboard example - DC motors
 
-from machine import Pin, PWM
 from time import sleep
 from util.pinout import set_pinout
 pinout = set_pinout()
 
-#from motor import *
-#moto_run(300) ## 300-1000
+from util.dcmotors import Motor, Steering
+motor_r = Motor(pinout.MOTOR_1A, pinout.MOTOR_2A, pinout.MOTOR_12EN)
+motor_l = Motor(pinout.MOTOR_3A, pinout.MOTOR_4A, pinout.MOTOR_34EN)
+steering = Steering(motor_l, motor_r)
 
-moto_L1 = Pin(pinout.MOTOR_1A, Pin.OUT)
-moto_L2 = Pin(pinout.MOTOR_2A, Pin.OUT)
-moto_L  = PWM(Pin(pinout.MOTOR_12EN, Pin.OUT), freq=500, duty = 0)
+speed = 800
 
-MOTO_R_Correction = 45
-moto_R3 = Pin(pinout.MOTOR_3A, Pin.OUT)
-moto_R4 = Pin(pinout.MOTOR_4A, Pin.OUT)
-moto_R  = PWM(Pin(pinout.MOTOR_34EN, Pin.OUT), freq=500, duty = 0)
-
-
-def moto_stop():
-    motors(0, 0)
-
-
-def moto_run(speed, forward=True):
-    motors(speed, speed, forward)
-
-
-def motors(speedL, speedR, forward=True):
-    moto_L1.value(forward)
-    moto_L2.value(not forward)
-    moto_L.duty(speedL)
-
-    moto_R3.value(not forward)
-    moto_R4.value(forward)
-    moto_R.duty(speedR)
-
-
-
-moto_run(300)
-sleep(2)
-moto_run(600)
-sleep(3)
-moto_run(900)
-sleep(5)
-moto_stop()
-
-
+steering.center(0)
+steering.center(speed)
+sleep(1)
+steering.center(-speed)
+sleep(1)
+steering.right(speed/2)
+sleep(1)
+steering.left(speed/3)
+sleep(1)
+steering.center(0)
