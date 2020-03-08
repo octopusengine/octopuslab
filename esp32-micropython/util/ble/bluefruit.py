11498990c8d21187b167cfce950ee4502c7449f9
Honza S. Copak <bonum.comunae@volny.cz>
2020-03-08 17:28:31 +0100

    bluefruit app

---
+++ esp32-micropython/util/ble/bluefruit.py
@@ -0,0 +1,16 @@
+# Blue fruit connect - mobile app.
+# import util.ble.bluefruit as bf
+# -> exist bf.UP ...
+
+# --- control pad ---
+UP =   b'!B516'
+DOWN = b'!B615'
+LEFT = b'!B714'
+RIGHT = b'!B813'
+F1 = b'!B11'
+F2 = b'!B219'
+F3 = b'!B318'
+F4 = b'!B417' 
+
+# --- color picker --- 
+# todo
