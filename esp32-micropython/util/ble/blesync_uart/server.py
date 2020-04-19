8a1608da3d4aa12983def09342bb07cbc573fa38
Honza S. Copak <bonum.comunae@volny.cz>
2020-04-19 12:30:44 +0200

    ble lib

---
+++ esp32-micropython/util/ble/blesync_uart/server.py
@@ -0,0 +1,26 @@
+import bluetooth
+
+import util.ble.blesync_server
+
+
+class UARTService(blesync_server.Service):
+    uuid = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
+
+    _tx = blesync_server.Characteristic(
+        bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
+        bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,
+    )
+
+    _rx = blesync_server.Characteristic(
+        bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
+        bluetooth.FLAG_WRITE,
+        buffer_size=100,
+        append=True
+    )
+
+    characteristics = (_tx, _rx)
+
+    on_message = _rx.on_message
+
+    def send(self, conn_handle, message):
+        self._tx.notify(conn_handle, message)
