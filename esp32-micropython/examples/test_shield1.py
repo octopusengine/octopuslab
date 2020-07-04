d7fe6a14ab8af6844ff1bf38cae2294bf6709994
octopusengine <honza.copak@gmail.com>
2020-07-04 16:35:52 +0200

    fix example

--- esp32-micropython/examples/test_shield1.py
+++ esp32-micropython/examples/test_shield1.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+print("--- examples/test_shield.py ---")
+print("-"*30)
 print("--- Led2 | Led3 ---")
 from components.led import Led
 l2 = Led(25)
@@ -19,6 +21,23 @@ print("--- OLED ---")
 from utils.octopus import oled_init
 oled = oled_init()
 
+print("--- Button ---")
+from components.button import Button
+btn_L = Button(34, release_value=1)
+btn_R = Button(35, release_value=1)
+
+@btn_L.on_press
+def on_press_L_button():
+    print("on_press_L_button")
+    piezzo.beep()
+    l2.toggle()
+
+@btn_R.on_press
+def on_press_R_button():
+    print("on_press_R_button")
+    piezzo.beep()
+    l3.toggle()
+
 print("--- ok ---")
 
 from components.buzzer.melody import jingle1
@@ -26,4 +45,5 @@ piezzo.play_melody(jingle1)
 l2.blink()
 l3.blink()
 oled.poweroff()
+print("-"*30)
 
