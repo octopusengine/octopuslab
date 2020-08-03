bf44e995b3181d939ef2967213f792bfef0c8ddf
Petr Kracík <petrkr@petrkr.net>
2020-08-03 22:26:58 +0200

    Examples: RTC clock DS3231 gettime

---
+++ esp32-micropython/examples/rtc_ds3231_gettime.py
@@ -0,0 +1,11 @@
+from machine import Pin, I2C
+from ds3231 import DS3231
+
+scl = Pin(16, Pin.OPEN_DRAIN, Pin.PULL_UP)
+sda = Pin(2, Pin.OPEN_DRAIN, Pin.PULL_UP)
+
+i2c = I2C(scl=scl, sda=sda, freq=400000)
+
+ds3231 = DS3231(i2c)
+
+ds3231.get_time()
