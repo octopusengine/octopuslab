e02cca95859f34e3e60ebdeefeae288f12dfbf15
octopusengine <honza.copak@gmail.com>
2020-09-24 17:08:40 +0200

    added Led info

--- esp32-micropython/utils/octopus_decor.py
+++ esp32-micropython/utils/octopus_decor.py
@@ -1,44 +1,30 @@
 # octopusLab "decorators" lib
 
-def octopus_debug(fnc):
-    print()
-    print("--- decorator --- @octopus_debug:")
-    import time
-    
-    def ff(*args, **kwargs):
-        start = time.time()
-        result = fnc(*args, **kwargs)
-        end = time.time() - start
-        try:
-           fname = fnc.__name__
-        except:
-           fname = "?"
-        print("=== function name: ", fname)
-        print("=== duration (sec.) --->", str(end))
-        return result
+def octopus_debug(ledon=False,info=True):
+   if ledon:
+         from components.led import Led
+         led = Led(2)
+         
+   def _octopus_debug(fnc):
+      import time
+      print("--- decorator --- @octopus_debug:")
+      #if ledon: led.blink()
+      
+      try:
+            fname = fnc.__name__
+      except:
+            fname = "?"
+      
+      def ff(*args, **kwargs):
+         if ledon: led.value(1)
+         start = time.time()
+         result = fnc(*args, **kwargs)
+         end = time.time() - start
+         if ledon: led.value(0)
+         
+         print("=== function name: ", fname)
+         print("=== duration (sec.) --->", str(end))
+         return result
 
-    return ff
-
-
-def octopus_led_on(fnc):
-    print()
-    print("--- decorator --- @octopus_led_on:")
-    import time
-    from components import Led
-    led = Led(2)
-    
-    def ff(*args, **kwargs):
-        led.value(1)
-        start = time.time()
-        result = fnc(*args, **kwargs)
-        end = time.time() - start
-        led.value(0)
-        try:
-           fname = fnc.__name__
-        except:
-           fname = "?"
-        print("=== function name: ", fname)
-        print("=== duration (sec.) --->", str(end))
-        return result
-
-    return ff
+      return ff
+   return _octopus_debug
