fa34c51e7b579e8a20515fafb34e18d51dffb8c1
octopusengine <honza.copak@gmail.com>
2020-11-29 21:59:56 +0100

    typo

--- esp32-micropython/examples/pubsub/ps_tm-disp-kbd.py
+++ esp32-micropython/examples/pubsub/ps_tm-disp-kbd.py
@@ -1,6 +1,5 @@
-# pub sub kbd+display example
+# pub sub kbd + display
 # EDU_KIT1 + TM1638 shield
-
 
 from time import sleep
 import pubsub
@@ -9,7 +8,7 @@ from octopus import ps_tm
 print("--- TM1638 - pubsub - example ---")
 
 print("display test")
-pubsub.publish('tm_display', "test 123")
+pubsub.publish('tm_display', "octopus")
 sleep(2)
 pubsub.publish('tm_display', "        ")
 
@@ -17,8 +16,8 @@ pubsub.publish('tm_display', "        ")
 # table1: matrix 4x4 - ABCD > K1 K2 K3 -
 btn_tab = {1:'E',2:'8',4:'4',8:'123',16:'C',32:'7',64:'3',128:'123',512:'0',1024:'6',2048:'2',8192:'9',16384:'5',32768:'1'}
 
-num = 0
 
+num = 0
 def add_digit(dig):
   global num
   try:
@@ -46,7 +45,3 @@ def kbd_action(btn_value):
     else:
         num = add_digit(btn_tab[btn_value])
         pubsub.publish('tm_display', format_digit(num))
-    
-    
-    
-
