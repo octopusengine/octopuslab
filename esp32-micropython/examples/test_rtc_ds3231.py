2a4f6179746e6838fd4d3574e5b6228bdfdcef09
Petr Kracík <petrkr@petrkr.net>
2021-02-16 15:05:27 +0100

    Test RTC: use HW i2c

--- esp32-micropython/examples/test_rtc_ds3231.py
+++ esp32-micropython/examples/test_rtc_ds3231.py
@@ -1,10 +1,10 @@
-from machine import Pin, I2C
 from ds3231 import DS3231
+from machine import Pin, I2C
 
 scl = Pin(16, Pin.OPEN_DRAIN, Pin.PULL_UP)
 sda = Pin(2, Pin.OPEN_DRAIN, Pin.PULL_UP)
 
-i2c = I2C(scl=scl, sda=sda, freq=400000)
+i2c = I2C(1, scl=scl, sda=sda, freq=100000)
 
 ds3231 = DS3231(i2c)
 
