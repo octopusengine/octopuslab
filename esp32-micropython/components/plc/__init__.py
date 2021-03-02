b5bc5a1e4e995436c5b97bc3a3088cc5099196fe
octopusengine <honza.copak@gmail.com>
2021-03-02 19:20:45 +0100

    pls modules

--- esp32-micropython/components/plc/__init__.py
+++ esp32-micropython/components/plc/__init__.py
@@ -7,6 +7,7 @@ class Input():
     def __init__(self):
         pass
 
+    @property
     def output(self):
         return self._value
 
@@ -37,22 +38,27 @@ class Operands():
     def inputs():
         raise "Not Implemented"
 
+    @property
     def output():
         raise "Not Implemented"
 
     value = property(get_value, set_value)
 
 
-class Operand_AND():
-    def __init__(self, inputs=[]):
-        self._inputs = inputs
+class Operand_AND:
+    def __init__(self, inputs=None):
+        if not inputs:
+            self._inputs = list()
+        else:
+            self._inputs = inputs
 
     def addInput(self, input):
         self._inputs.append(input)
 
+    @property
     def output(self):
         for i in self._inputs:
-            val = i.output()
+            val = i.output
             if not val:
                 return False
 
@@ -60,20 +66,25 @@ class Operand_AND():
 
 
 class Operand_NAND(Operand_AND):
+    @property
     def output(self):
-        return not super().output()
+        return not super().output
 
 
-class Operand_OR():
-    def __init__(self, inputs=[]):
-        self._inputs = inputs
+class Operand_OR:
+    def __init__(self, inputs=None):
+        if not inputs:
+            self._inputs = list()
+        else:
+            self._inputs = inputs
 
     def addInput(self, input):
         self._inputs.append(input)
 
+    @property
     def output(self):
         for i in self._inputs:
-            val = i.output()
+            val = i.output
             if val:
                 return True
 
@@ -81,37 +92,99 @@ class Operand_OR():
 
 
 class Operand_NOR(Operand_OR):
+    @property
     def output(self):
-        return not super().output()
+        return not super().output
 
 
 class Operand_NOT(Operands):
     def __init__(self, input):
         self._input = input
 
+    @property
     def output(self):
-        return not self._input.output()
+        return not self._input.output
 
 
-class Override_FIXED():
-    def __init__(self, value):
-        self._value = value
-
-    def output(self):
-        return self._value
-
-
-class Override_DYNAMIC():
+class Override():
     def __init__(self, input):
         self._input = input
-        self._value = False
         self._enabled = False
 
+    @property
+    def enabled(self):
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value):
+        self._enabled = value
+
+    @property
     def output(self):
         if self._enabled:
             return self._value
+        else:
+            return self._input.output
 
-        return self._input.output()
+
+class Override_FIXED(Override):
+    def __init__(self, input, value):
+        self._value = value
+        super().__init__(input)
+
+
+class Override_DYNAMIC(Override):
+    def __init__(self, input):
+        self._value = False
+        super().__init__(input)
+
+    @property
+    def value(self):
+        pass
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+a = Operand_AND()
+na = Operand_NAND()
+or1 = Operand_OR()
+
+i1 = DummyInput(True)
+od1 = Override_DYNAMIC(i1)
+i2 = DummyInput(0)
+i3 = DummyInput("asd")
+i4 = DummyInput(True)
+
+a.addInput(i1)
+a.addInput(i2)
+a.addInput(i3)
+
+na.addInput(i1)
+na.addInput(i2)
+na.addInput(i3)
+
+or1.addInput(od1)
+or1.addInput(a)
+
+nt = Operand_NOT(a)
+
+print("AND: {}".format(a.output))
+print("NAND: {}".format(na.output))
+print("NOT: {}".format(nt.output))
+print("OR: {}".format(or1.output))
+print("OD: {}".format(od1.output))
+
+
+i4._value = False
+od1.enabled = True
+print("OR: {}".format(or1.output))
+od1.value = False
+print("OR: {}".format(or1.output))
+od1.enabled = False
+print("OR: {}".format(or1.output))
+
 
 
 """
@@ -138,19 +211,18 @@ or1.addInput(a)
 
 nt = Operand_NOT(a)
 
-print("AND: {}".format(a.output()))
-print("NAND: {}".format(na.output()))
-print("NOT: {}".format(nt.output()))
-print("OR: {}".format(or1.output()))
-print("OD: {}".format(od1.output()))
+print("AND: {}".format(a.output))
+print("NAND: {}".format(na.output))
+print("NOT: {}".format(nt.output))
+print("OR: {}".format(or1.output))
+print("OD: {}".format(od1.output))
 
 
 i4._value = False
-od1._enabled = True
-od1._value = True
-print("OR: {}".format(or1.output()))
-od1._value = False
-print("OR: {}".format(or1.output()))
-od1._enabled = False
-print("OR: {}".format(or1.output()))
+od1.enabled = True
+print("OR: {}".format(or1.output))
+od1.value = False
+print("OR: {}".format(or1.output))
+od1.enabled = False
+print("OR: {}".format(or1.output))
 """
--- esp32-micropython/examples/test_plc.py
+++ esp32-micropython/examples/test_plc.py
@@ -3,9 +3,11 @@ from utils.octopus_lib import i2c_init
 from components.lm75 import LM75B
 from components.i2c_expander import Expander8
 from utils.bits import neg, reverse, int2bin, get_bit, set_bit
+from components.plc import *
 
 IN1, IN2, IN3, IN4     = 0, 1, 2, 3
 OUT1, OUT2, OUT3, OUT4 = 4, 5, 6, 7
+# OUT4 - signalization
 
 print("--- PLC shield ---")
 print("-"*50)
@@ -21,7 +23,7 @@ temp = lm.get_temp()
 print("- Temp: ", temp)
 
 exp8 = Expander8(34)
-print("- PCF:", print(exp8.read()))
+print("- PCF:", exp8.read())
 
 # bits4 = "0101"
 # exp8.write_8bit(bits4)
@@ -30,17 +32,52 @@ for i in range(3):
     exp8.write_8bit(0)
     sleep(0.1)
     exp8.write_8bit(255)
-    sleep(0.5)
+    sleep(0.1)
     
 exp8.write_8bit(set_bit(255,OUT1,0))
-sleep(1)
+sleep(0.2)
 exp8.write_8bit(set_bit(255,OUT2,0))
-sleep(1)
+sleep(0.2)
 exp8.write_8bit(set_bit(255,OUT3,0))
-sleep(1)
+sleep(0.2)
 exp8.write_8bit(set_bit(255,OUT4,0))
 
-sleep(1)
+sleep(0.3)
 exp8.write_8bit(255)
 
 print("- EEPROM:")
+# todo
+
+print("- modules")
+in1 = DummyInput()
+in2 = DummyInput()
+in3 = DummyInput()
+
+gate_or = Operand_OR()
+gate_or.addInput(in1)
+gate_or.addInput(in2)
+gate_or.addInput(in3)
+
+gate_and = Operand_AND()
+gate_and.addInput(in1)
+gate_and.addInput(in2)
+
+
+for test in range(10000):
+    #in8 = exp8.read()
+    """
+    temp8 = 0
+    exp8.write_8bit(set_bit(temp8,OUT4,0))
+    sleep(0.1)
+    exp8.write_8bit(set_bit(temp8,OUT4,1))
+    sleep(0.001)
+    """
+    
+    in8 = exp8.read()
+    in1._value = int(get_bit(in8,0))
+    in2._value = int(get_bit(in8,1))
+    in3._value = int(get_bit(in8,2))
+    print(in1.output, in2.output, in3.output, "->", gate_or.output, gate_and.output)
+    exp8.write_8bit(set_bit(255,OUT1,gate_or.output))
+
+    sleep(0.1)
