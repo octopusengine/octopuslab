8fafeeef7c05fe08905b8657dcc6a43784379291
Petr Kracík <petrkr@petrkr.net>
2021-03-07 12:11:36 +0100

    PLC test component: test RS Pulse

--- esp32-micropython/tests/test_plc_component.py
+++ esp32-micropython/tests/test_plc_component.py
@@ -1,9 +1,12 @@
 from components.plc.inputs.dummy import PLC_input_dummy
 from components.plc.inputs.fixed import plc_input_fixed_high, plc_input_fixed_low
 from components.plc.elements.rs import PLC_element_RS
+from components.plc.elements.rs_pulse import PLC_element_RS_pulse
 from components.plc.operands.op_and import PLC_operand_AND
 from components.plc.operands.op_or import PLC_operand_OR
 from components.plc.operands.op_not import PLC_operand_NOT
+
+from utime import sleep
 
 
 i1 = PLC_input_dummy(False)
@@ -18,6 +21,8 @@ o1.add_input(plc_input_fixed_high)
 n1 = PLC_operand_NOT(i1)
 
 rs = PLC_element_RS(i1, i2)
+
+rsp = PLC_element_RS_pulse(i1, i2)
 
 print("Init: ", rs.output)
 i1.value = True
@@ -41,3 +46,10 @@ i1.value = True
 print("AND: ", a1.output)
 print("OR: ", o1.output)
 print("NOT: ", n1.output)
+print("RS Pulse {}".format(rsp.output))
+print("RS Pulse {}".format(rsp.output))
+
+i1.value = False
+for i in range(0,20):
+    print("RS Pulse ({}): {}".format(i, rsp.output))
+    sleep(1)
