e052b6d22b74c3f790aa5d1fa708d74e897f519b
Petr Kracík <petrkr@petrkr.net>
2021-11-14 00:58:32 +0100

    Keypad generic functions for both classes

--- esp32-micropython/components/i2c_keypad/__init__.py
+++ esp32-micropython/components/i2c_keypad/__init__.py
@@ -1,5 +1,5 @@
 # OctopusLAB (c) 2021
-# Version 0.2
+# Version 0.3
 
 # by Petr Kracik
 
@@ -10,6 +10,7 @@ class Keypad():
     def __init__(self, i2c, address=0x21, invert=False):
         self.i2c = i2c
         self.address = address
+        self.bussize = 8
 
         self.KEYPAD = [
         ['1', '2', '3', 'A'],
@@ -28,7 +29,12 @@ class Keypad():
 
     def pin_read(self, pinNum):
         mask = 0x1 << pinNum
-        pinVal = self.i2c.readfrom(self.address, 1)[0]
+
+        value = self.i2c.readfrom(self.address, self.bussize // 8)
+        pinVal = value[0]
+
+        if self.bussize > 8:
+            pinVal += value[1] << 8
 
         pinVal &= mask
         if (pinVal == mask):
@@ -39,12 +45,15 @@ class Keypad():
 
     def getKey(self):
         c = 0
-        tmp = bytearray(1)
+        tmp = bytearray(self.bussize // 8)
 
         for i in self.ROW:
             c += 1 << i
 
         tmp[0] = c
+        if self.bussize > 8:
+            tmp[1] = c >> 8
+
         self.i2c.writeto(self.address, tmp)
 
         rowVal = -1
@@ -61,6 +70,9 @@ class Keypad():
             c += 1 << i
 
         tmp[0] = c
+        if self.bussize > 8:
+            tmp[1] = c >> 8
+
         self.i2c.writeto(self.address, tmp)
 
         colVal = -1
@@ -106,6 +118,7 @@ class Keypad16(Keypad):
     def __init__(self, i2c, address=0x20, invert=False):
         self.i2c = i2c
         self.address = address
+        self.bussize = 16
 
         self.KEYPAD = [
         ['1', '2', '3', 'A'],
@@ -120,58 +133,3 @@ class Keypad16(Keypad):
         else:
             self.ROW    = [0,1,2,3]
             self.COLUMN = [4,5,6,7]
-
-    def pin_read(self, pinNum):
-        mask = 0x1 << pinNum
-
-        value = self.i2c.readfrom(self.address, 2)
-        pinVal = value[0]
-        pinVal += value[1] << 8
-
-        pinVal &= mask
-        if (pinVal == mask):
-            return 1
-        else:
-            return 0
-
-    def getKey(self):
-        c = 0
-        tmp = bytearray(2)
-
-        for i in self.ROW:
-            c += 1 << i
-
-        tmp[0] = c
-        tmp[1] = c >> 8
-
-        self.i2c.writeto(self.address, tmp)
-
-        rowVal = -1
-        for i in range(len(self.ROW)):
-            tmpRead = self.pin_read(self.ROW[i])
-            if tmpRead == 0:
-                rowVal = i
-
-        if rowVal < 0 or rowVal > len(self.ROW) - 1:
-            return None
-
-        c = 0
-        for i in self.COLUMN:
-            c += 1 << i
-
-        tmp[0] = c
-        tmp[1] = c >> 8
-
-        self.i2c.writeto(self.address, tmp)
-
-        colVal = -1
-        for j in range(len(self.COLUMN)):
-            tmpRead = self.pin_read(self.COLUMN[j])
-            if tmpRead == 0:
-                colVal=j
-
-        if colVal < 0 or colVal > len(self.COLUMN) - 1:
-            return None
-
-        # Return the value of the key pressed
-        return self.KEYPAD[rowVal][colVal]
