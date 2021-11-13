2be23595cff2e71dcea0c0fc55d84ff1e1139d40
Petr Kracík <petrkr@petrkr.net>
2021-11-13 21:12:37 +0100

    Keypad added 4x4 keypad layout for 16bit exp

--- esp32-micropython/components/i2c_keypad/__init__.py
+++ esp32-micropython/components/i2c_keypad/__init__.py
@@ -76,6 +76,32 @@ class Keypad():
         return self.KEYPAD[rowVal][colVal]
 
 
+# 5x4 keypad
+#self.KEYPAD = [
+#            ['F1', 'F2', '#', '*'],
+#            ['1', '2', '3', '^'],
+#            ['4', '5', '6', 'v'],
+#            ['7', '8', '9', 'ESC'],
+#            ['<', '0', '>', 'ENT']
+#            ]
+#        self.ROW    = [0,1,2,3,4]
+#        self.COLUMN = [8,7,6,5]
+
+# 4x4 keypad
+#self.KEYPAD = [
+#            ['1', '2', '3', 'A'],
+#            ['4', '5', '6', 'B'],
+#            ['7', '8', '9', 'C'],
+#            ['*', '0', '#', 'D']
+#            ]
+#        self.ROW    = [0,1,2,3]
+#        self.COLUMN = [4,5,6,7]
+#Invert
+#        self.ROW    = [7,6,5,4]
+#        self.COLUMN = [3,2,1,0]
+
+
+
 class Keypad16(Keypad):
     def __init__(self, i2c, address=0x20, invert=False):
         self.i2c = i2c
@@ -83,15 +109,14 @@ class Keypad16(Keypad):
 
         if not invert:
             self.KEYPAD = [
-            ['F1', 'F2', '#', '*'],
-            ['1', '2', '3', '^'],
-            ['4', '5', '6', 'v'],
-            ['7', '8', '9', 'ESC'],
-            ['<', '0', '>', 'ENT']
+            ['1', '2', '3', 'A'],
+            ['4', '5', '6', 'B'],
+            ['7', '8', '9', 'C'],
+            ['*', '0', '#', 'D']
             ]
 
-        self.ROW    = [0,1,2,3,4]
-        self.COLUMN = [8,7,6,5]
+        self.ROW    = [0,1,2,3]
+        self.COLUMN = [4,5,6,7]
 
     def pin_read(self, pinNum):
         mask = 0x1 << pinNum
