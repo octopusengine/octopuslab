7ebe4dd71835e842f56a84b0e52d6db5dfd4201d
Petr Kracík <petrkr@petrkr.net>
2023-02-14 15:34:08 +0100

    Test gyro: Use mpu6500 library

--- esp32-micropython/examples/test_gyro.py
+++ esp32-micropython/examples/test_gyro.py
@@ -2,30 +2,19 @@
 
 from time import sleep, sleep_ms, ticks_ms
 from machine import Pin, I2C
-import mpu6050
+from mpu6500 import MPU6500
 
 print("init")
-i2c = I2C(scl=Pin(22), sda=Pin(21))     #initializing the I2C method for ESP32
-#i2c = I2C(scl=Pin(5), sda=Pin(4))       #initializing the I2C method for ESP8266
-mpu= mpu6050.accel(i2c)
-
-"""
-vals["AcX"] = self.bytes_toint(raw_ints[0], raw_ints[1])
-vals["AcY"] = self.bytes_toint(raw_ints[2], raw_ints[3])
-vals["AcZ"] = self.bytes_toint(raw_ints[4], raw_ints[5])
-vals["Tmp"] = self.bytes_toint(raw_ints[6], raw_ints[7]) / 340.00 + 36.53
-vals["GyX"] = self.bytes_toint(raw_ints[8], raw_ints[9])
-vals["GyY"] = self.bytes_toint(raw_ints[10], raw_ints[11])
-vals["GyZ"] = self.bytes_toint(raw_ints[12], raw_ints[13])
-"""
+i2c = I2C(0, sda=Pin(0), scl=Pin(1))
+mpu = MPU6500(i2c)
 
 print("start")
 t0 = ticks_ms
 
 for i in range(1000):
-   mpu.get_values()
-   vals = mpu.get_values()
-   print(vals["AcX"],vals["AcY"],vals["AcZ"])
+   gyro = mpu.gyro
+   accel = mpu.acceleration
+   print(gyro[0],gyro[1],gyro[2], accel[0], accel[1], accel[2])
    sleep_ms(50)
    
 print("stop", ticks_ms - t0)
